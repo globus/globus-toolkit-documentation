5e97c05c338ee0cb7510e54cdaa25f1074d8d260
liming <liming>
2005-01-25 21:50:39 +0000

    Added as part of Globus Primer

---
+++ gridware/computation/mpich-g2.php
@@ -0,0 +1,64 @@
+<?php
+$title = "Grid Ecosystem - MPICH-G2";
+$section = "section-4";
+include_once( "../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+include($SITE_PATHS["SERV_INC"] . "app-info.inc");
+?>
+<div id="left">
+<?php include($SITE_PATHS["SERV_INC"].'software.inc'); ?>
+</div>
+
+<div id="main">
+
+<p><a href="../computation-components.php"><< Components for Grid Computation</a></p>
+
+<h1 class="first">MPICH-G2</h1>
+
+<p>
+<img src='MPICH-G2.jpg' style='float: right; margin-left: 0.3em;'>
+MPICH-G2 is a Grid-enabled implementation of the popular MPI (Message Passing Interface) 
+framework. MPI is especially useful for problems that can be broken up into several
+processes running in parallel, with information exchanged between the processes as needed.
+The MPI programming environment handles the details of starting and shutting down 
+processes, coordinating execution (supporting barriers and other IPC metaphors), and 
+passing data between the processes.
+</p>
+
+<p>
+MPICH-G2 uses Grid capabilities for starting processes on remote systems, for staging
+executables and data to remote systems, and for security. MPICH-G2 also selects 
+the appropriate communication method (high-performance local interconnect, Internet
+protocol, etc.) depending on whether messages are being passed between local or remote
+systems.
+</p>
+
+<p>
+MPICH-G2 implements the MPI v1.1 specification.
+</p>
+
+<?
+$software = "<a href='http://www.globus.org/mpi/'>MPICH-G2</a>";
+$developer = "<a href='http://www.niu.edu/'>Northern Illinois University</a> and 
+              <a href='http://www.mcs.anl.gov/'>Argonne National Laboratory</a>";
+$distros = "Download from the <a href='http://www-unix.mcs.anl.gov/mpi/mpich/'>MPICH website</a>";
+$contact = "<a href='mailto:mpi@globus.org'>mpi@globus.org</a><br>
+            (must be <a href='http://www.globus.org/about/subscriptions.html'>subscribed</a> before posting to the list)";
+
+
+// use the function below once problem is discovered, remove the two lines above
+app_info($software, $developer, $distros, $contact);
+
+?>
+
+<p></p>
+
+<p style="clear: left;"><a href="../computation-components.php"><< Components for Grid Computation</a></p>
+
+</div>
+
+<?
+
+include($SITE_PATHS["SERV_INC"].'footer.inc');
+
+?>
