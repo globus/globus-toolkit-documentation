f0663cce19ab4e8e5b5b4602bfd45b28afff0d2e
liming <liming>
2005-03-29 22:36:18 +0000

    New look!  and updates to reflect new web structure

--- contacts.php
+++ contacts.php
@@ -6,23 +6,25 @@ $section = "section-2";
 include_once( "include/local.inc" );
 include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
 ?>
+<!--
 <div id="left">
 <?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
 </div>
+-->
 
 <div id="main">
-<!-- content STARTS here -->
  
-<p>Here is how to communicate
-    with the Globus Alliance.</p>
+<h1 class="first">Contact Us</h1>
 
-<h1> Technical Questions and Problem Reporting </h1>
-    <p>The Globus Alliance has established a Bugzilla server for
-tracking bugs, feature requests, and questions about the Globus
-Toolkit<sup><font size=-2>TM</font></sup> and related issues.</p>
+<h2> Technical Questions and Problem Reporting </h2>
+
+<p>If you are using the Globus<sup>&reg;</sup> Toolkit and you want to report a bug 
+or make a feature request, or if you want to ask a question and 
+have it entered in our automated tracking system, use the Globus 
+Toolkit Bugzilla system.</p>
 
 <p>To establish a new account, go to the <a
-href="http://bugzilla.globus.org/bugzilla/"> Globus Alliance Bugzilla
+href="http://bugzilla.globus.org/bugzilla/">Globus Toolkit Bugzilla
 page </a> and select "Open a new account".  You will then be able to
 submit new bugs, and query existing bugs.  There is a <a
 href="http://bugzilla.globus.org/bugzilla/quicksearch.html">short
@@ -30,25 +32,28 @@ bug-query form</a> that lets you search by keyword and a <a
 href="http://bugzilla.globus.org/bugzilla/query.cgi">more involved
 interface</a> that lets you search on many different fields.</p>
 
-    <p>Our <a href="email-archive-search.php">searchable archive</a>
+
+<h2>Mailing Lists</h2>
+
+    <p>The Globus Alliance maintains a number of mailing lists for people who
+    are active in the Grid community or who use the Globus Toolkit. Joining a list
+    is an excellent way to stay in touch with others who share your interest 
+    in the Grid.</p>
+
+    <p>Visit the Globus <a href="subscriptions.php">Subscription
+Center</a> to subscribe to or unsubscribe from these mailing lists.</p>
+
+<p>Our <a href="email-archive-search.php">searchable archive</a>
 of Globus Alliance mailing lists provides a rich source of information
 about the Globus Toolkit and related technologies. The <a
 href="http://www-unix.globus.org/mail_archive/discuss/threads.html">discuss@globus.org</a>
 mailing list in particular is an excellent way to get help with the
-Globus Toolkit. See below for more information about subscribing to
-this list.</p>
+Globus Toolkit.</p>
 
-<h1>Mailing Lists</h1>
-    <p>The Globus Alliance maintains a number of mailing lists for users of 
-    the Globus Toolkit.&nbsp; These provide an excellent way to stay in touch
-    with others who are using the Toolkit and who share your interests in Grid
-    computing.</p>
-    <p>Visit the Globus <a href="subscriptions.php">Subscription
-Center</a> to subscribe to or unsubscribe from these mailing lists.</p>
+<h2>Alliance Addresses</h2>
 
-<h1>Alliance Addresses</h1>
-
-    <p>The following addresses may be used for directing questions about the Globus  Alliance to us.</p>
+    <p>If you want to communicate directly with members of the Globus Alliance, you may
+       use one of the following addresses.</p>
 
 
     <table border="0" cellspacing="0" cellpadding="5" width="100%">
@@ -64,32 +69,6 @@ Center</a> to subscribe to or unsubscribe from these mailing lists.</p>
         <td>Comments about this website</td>
       </tr>
     </table>
-<br>
-   <h1>Principal Investigators</h1>
-
-   <table border="0" cellspacing="0" cellpadding="5" width="100%">
-      <tr valign="top">
-        <td width="50%"><a href="http://www.mcs.anl.gov/people/foster">Ian Foster</a><br>
-
-        <small>Mathematics and Computer Science<br>
-        Argonne National Laboratory<br>
-        9700 Cass Ave<br>
-        Argonne, IL, 60439<br>
-        Tel: 630 252-4619<br>
-        Fax: 630 252-5986<br>
-
-        <a href="mailto:foster@mcs.anl.gov">foster@mcs.anl.gov</a></small></td>
-        <td width="50%"><a href="http://www.isi.edu/~carl">Carl Kesselman</a><br>
-        <small>USC/Information Sciences Institute<br>
-        4676 Admiralty Way, Suite 1001<br>
-        Marina del Rey, CA 90292-6695<br>
-        Tel: 310 822-1511 x338<br>
-
-        Fax: 310 823-6714<br>
-        <a href="mailto:carl@isi.edu">carl@isi.edu</small></a></td>
-      </tr>
-    </table>
- <br>
 
    
 </div>
