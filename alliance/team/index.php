f5f99e1f78299379fa9a79ce25c12ec5e84b688d
liming <liming>
2005-03-30 21:39:15 +0000

    First versions

---
+++ alliance/team/index.php
@@ -0,0 +1,246 @@
+<?php
+
+$title = "Globus: Alliance - Paricipants";
+$section = "section-2";
+include_once( "../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+<!--
+<div id="left">
+<?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+</div>
+-->
+
+<div id="main">
+<!-- content STARTS here -->
+<h1> Globus Alliance Participants</h1>
+
+<ol>
+<li><a href="#team">Our Team</a>
+<li><a href="#affiliates">Affiliates</a>
+</ol>
+
+
+<h1><a name="team" class="title">The Globus Team</a></h1>
+
+<p>The following people are currently members of the core Globus Alliance team.  (See also our <a href="alumni.php">  alliance alumni</a>.)</p>
+
+    <p><a href="http://www.mcs.anl.gov">Argonne National Laboratory</a>
+	<ul>
+	<li>Bill Allcock</li>
+	<li>Rachana Anathakrishnan</li>
+	<li><a href="http://www.mcs.anl.gov/~bester/">Joe Bester</a></li>
+	<li>Eric Blau</li>
+
+	<li>John Bresnahan</li>
+	<li>Lisa Childers</li>
+	<li>Doug Engert</li>
+	<li><a href="http://www.mcs.anl.gov/~foster/">Ian Foster</a></li>
+	<li>Bob Gaffaney</li>
+	<li><a href="http://www.mcs.anl.gov/~gawor">Jarek Gawor</a></li>
+
+	<li>Paul Hubbard</li>
+	<li>Joe Insley</li>
+	<li>Kate Keahey</li>
+	<li>Raj Kettimuthu</li>
+	<li>Peter Lane</li>
+	<li>Sam Lang</li>
+
+	<li><a href="http://www.mcs.anl.gov/~gregor/">Gregor von Laszewski</a></li>
+	<li><a href="http://www-unix.mcs.anl.gov/~liming/">Lee Liming</a></li>
+	<li>Joe Link</li>
+	<li>Mike Link</li>
+	<li>Ravi Madduri</li>     
+	<li>Stuart Martin</li>
+
+	<li>Veronika Nefedova</li>
+	<li>Gigi Rohder</li>
+	<li>Jennifer Schopf</li>
+	<li>Frank Siebenlist</li>
+	<li><a href="http://www-fp.mcs.anl.gov/~tuecke">Steve Tuecke</a></li>
+	<li>Rich Wellner</li>
+
+	<li>Mike Wilde</li>
+ 	<li>Julie Wulf</li>
+    </ul>
+
+    <p><a href="http://www.cs.uchicago.edu/">University of Chicago</a></p>
+    
+    <ul>
+<!--<li><a href="http://people.cs.uchicago.edu/~dangulo/">Dave Angulo</a></li>-->
+	<li>Charles Bacon
+      <li>Scott Gose
+      <li>Samuel Meder</li>
+
+      <li>Jens-Soenke Voeckler
+      </ul>
+    
+    <p><a href="http://www.isi.edu">University of Southern California Information Sciences
+    Institute</a>
+    
+    <ul>
+      <li>Alain Andrieux</li>
+      <li>Shishir Bharathi</li>
+      <li><a href="http://www.isi.edu/~annc">Ann L. Chervenak</a></li>
+
+      <li>Ben Clifford</li>
+      <li><a href="http://www.isi.edu/~karlcz">Karl Czajkowski</a></li>
+      <li>Mike D'Arcy</li>
+      <li><a href="http://www.isi.edu/~deelman">Ewa Deelman</a></li>
+      <li>Larry Flon</li>
+      <li><a href="http://www.isi.edu/~sridhar">Sridhar Gullapalli</a></li>
+
+      <li><a href="http://www.isi.edu/~carl">Carl Kesselman</a></li>
+      <li>June-Sup Lee</li>
+      <li>Guarang Mehta</li>
+      <li>John McGee</li>
+      <li>Nosa Olomu
+      <li>Laura Pearlman</li>
+      <li>Mats Rynge</li>
+
+      <li>Tatyana Ryutov</li>
+      <li>Robert Schuler</li>
+      <li>Mei-Hui Su</li>
+      <li><a href="http://www.isi.edu/~thiebaux">Marcus Thiebaux</a>
+	<li>Hongsuda Tangmunarunkit</li>
+	<li>Karan Vahi</li>
+
+	<li>Cristina Williams</li>
+	
+    </ul> 
+    
+    <p><a href="http://www.ed.ac.uk/">University of Edinburgh</a><ul>
+      <li>Mario Antonioletti (EPCC)</li>
+      <li>Malcolm Atkinson (National e-Science Centre)</li>
+      <li>Rob Baxter (EPCC)</li>
+      <li>Ally Hume (EPCC)</li>
+
+      <li>Neil Chue Hong (EPCC)</li>
+      <li>Mike Jackson (EPCC)</li>
+      <li>Amy Krause (EPCC)</li>
+      <li>Mark Parsons (EPCC)</li>
+      <li>Tom Sugden (EPCC)</li>
+      <li>Martin Westhead (EPCC)</li>
+
+      </ul>
+    
+    <p>Swedish Royal Institute of Technology<ul>
+        <li>Olle Mulmo</li>
+        <li>Thomas Sandholm</li>
+        </ul>
+    
+    <p><a href="http://www.hpclab.niu.edu/">High Performance Computing Laboratory Northern
+    Illinois University</a>
+	<ul>
+
+	<li><a href="http://www.cs.niu.edu/~karonis/">Nicholas T. Karonis</a></li>
+	</ul>
+
+
+
+	<h1><a name="affiliates" class="title">The Globus Academic Affiliates Program</a></h1>
+
+<p>In addition to <a href="http://www.mcs.anl.gov">Argonne</a>,
+<a href="http://www.isi.edu">ISI</a>,  the 
+<a href="http://dsl.cs.uchicago.edu/">University of Chicago</a>, the 
+<a href="http://www.ed.ac.uk/"> University of Edinburgh</a>, and the 
+Swedish <a href="http://www.pdc.kth.se/">Center for Parallel Computers</a>,
+ the following organizations are part of the 
+<a href="news/prGAannounce.html">Academic Affiliates Program announced</a> upon creation of the Globus Alliance in September 2003. The Globus Academic Affiliates program recognizes major contributions to the Globus Toolkit by other organizations, as contributors or users. </p>
+
+<table border="1" width="436">
+<tr>
+<td width="161"> <p style="margin-top: 0; margin-bottom: 0"><b>Europe:</b><td>
+<td width="259" align="center"></td>
+</tr>
+ 
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0"><a href="http://www.aei-potsdam.mpg.de/">Max Planck Institute for Gravitational Physics</a> -- Albert Einstein Institute (Germany)</td>
+<td width="259" align="center"><a href="images/AEI_Logo.jpg"><img border="0" src="images/AEI_Logo_small.jpg" xthumbnail-orig-image="images/AEI_Logo.jpg" width="69" height="81"></a></td>
+</tr>
+
+<tr>
+<td width="161"> <p style="margin-top: 0; margin-bottom: 0"><a href="http://www.ic.ac.uk/">Imperial College</a> (UK)</font></td>
+<td width="259" align="center"><a href="images/ImperialCollege.jpg"><img border="0" src="images/ImperialCollege_small.jpg" xthumbnail-orig-image="images/ImperialCollege.jpg" width="210" height="88"></a></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0"> <a href="http://www.man.poznan.pl/pcss/public/main/">Poznan Supercomputing and Networking  Center</a> (Poland)<br></td>
+
+<td width="259" align="center"> <a href="images/logo_PSNC.jpg"> <img border="0" src="images/logo_PSNC_small.jpg" xthumbnail-orig-image="images/logo_PSNC.jpg" width="210" height="79"></a></td>
+ </tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0"><b>Asia-Pacific:</b></td>
+<td width="259" align="center"></td>
+</tr>
+
+<tr>
+<td width="161"> <p style="margin-top: 0; margin-bottom: 0">Monash University (Australia)</td>
+ <td width="259" align="center"><a href="images/Monash_1-CMYK.jpg"> <img border="0" src="images/Monash_1-CMYK_small.jpg" xthumbnail-orig-image="images/Monash_1-CMYK.jpg" width="310" height="36"></a></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0">National Institute of Advanced Industrial Science and Technology’s Grid Technology and Research Center (Japan)</td>
+<td width="259" align="center"><a href="images/aist-logo.jpg"><img border="0" src="images/aist-logo_small.jpg" xthumbnail-orig-image="images/aist-logo.jpg" width="182" height="64"></a></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0">Tokyo Institute of Technology (Japan)</td>
+<td width="259" align="center"></td>
+</tr>
+
+<tr>
+<td width="161"> <p style="margin-top: 0; margin-bottom: 0"><b>U.S.:</b></td>
+ <td width="259" align="center"></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0">Indiana University</td>
+<td width="259" align="center"><a href="http://www.indiana.edu/~afrotc/Images/redblackblockiu.gif"><img src="images/redblackblockiu_small.gif" xthumbnail-orig-image="http://www.indiana.edu/~afrotc/Images/redblackblockiu.gif" border="0" width="93" height="121"></a></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0">Lawrence Berkeley National Laboratory</td>
+<td width="259" align="center"><a href="images/LBNL_Logo.jpg"><img border="0" src="images/LBNL_Logo_small.jpg" xthumbnail-orig-image="images/LBNL_Logo.jpg" width="163" height="96"></a></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0">Louisiana State University</td><td width="259" align="center"><img border="0" src="images/lsulogo.jpg" width="87" height="124"></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0"><font face="Verdana" SIZE="2">National Center for Supercomputing Applications</td>
+<td width="259" align="center"><a href="http://portal.ncsa.uiuc.edu/SJK/carlton/newlogo/newlogo.gif"> <img src="images/newlogo_small.gif" xthumbnail-orig-image="http://portal.ncsa.uiuc.edu/SJK/carlton/newlogo/newlogo.gif" border="0" width="167" height="103"></a></td>
+</tr>
+
+<tr>
+<td width="161"><p>Northern Illinois University</td>
+<td width="259" align="center"> <img border="0" src="images/NIU%20logo.jpg" width="111" height="188"></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0">San Diego Supercomputer Center</td>
+<td width="259" align="center"><a href="images/GAaffi2.gif"><img border="0" src="images/GAaffi1.gif" xthumbnail-orig-image="images/GAaffi2.gif" width="210" height="49"></a></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0">Texas Advanced Computing Center at the University of Texas</td>
+<td width="259" align="center"> <img border="0" src="images/TACC.gif" width="294" height="43"></td>
+</tr>
+
+<tr>
+<td width="161"><p style="margin-top: 0; margin-bottom: 0">University of California at Santa Barbara</td>
+<td width="259" align="center"><img border="0" src="images/ucsbseal.jpg" width="113" height="112"></td>
+</tr>
+
+<tr>
+<td width="161"> <p style="margin-top: 0; margin-bottom: 0">University of Wisconsin Condor group</td>
+<td width="259" align="center"><a href="images/NewCondorLogo.jpg"><img border="0" src="images/NewCondorLogo_small.jpg" xthumbnail-orig-image="images/NewCondorLogo.jpg" width="210" height="40"></a></td>
+</tr>
+</table>
+
+<!-- content ENDS here -->
+
+</div>
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
