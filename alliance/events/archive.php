4cdec408df307ba9cf63aca1c3f69adef9cc038d
liming <liming>
2005-04-05 21:31:40 +0000

    Initial version

---
+++ alliance/events/archive.php
@@ -0,0 +1,187 @@
+<?php
+
+$title = "Globus: Meetings and Events Archive";
+$section = "section-2";
+include_once( "../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+<!--
+<div id="left">
+<?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+</div>
+-->
+
+<div id="main">
+
+<h1>Meetings and Events Archive</h1>
+
+<p>These events are <em>ancient history!</em> Please see our 
+<a href="index.php">Meetings and Events</a> page for current and recent 
+event information.</p>
+
+<hr>
+
+<img border="0" src="../images/logo.gif" align="right" width="212" height="145">
+
+<p><b>The Globus Alliance at SC2004</b></p>
+
+<p>See <b>Globus@Work</b> at SC2004!</p>
+
+<p><small>David L. Lawrence Convention Center, Pittsburgh PA, November 6-12, 2004</small></p>
+
+<p>Visit us at booth R339 (Argonne National Laboratory) and see how the GlobusToolkit is being put to work in exciting applications that advance scienceand engineering.</p>
+
+<p>During the conference, the Globus Toolkit will be demonstrated in action by
+numerous application demonstrations around the exhibit hall.  The Globus Project
+will also be represented by paper presentations, tutorials, posters, and other
+conference activities.</p>
+
+<ul>
+<li><a href="sc2004-program.php#TechnicalPapersAtSC02">Technical Papers at SC2004</a>
+<li><a href="sc2004-program.php#TechnicalPapersAtGrid2004">Technical Papers at Grid2004</a>
+<li><a href="sc2004-program.php#SC04Tutorials">Tutorials</a>
+<li><a href="sc2004-program.php#SC04ConferencePosters">Poster Presentations</a>
+<li><a href="sc2004-demos.php">Live Technology Demonstrations</a>
+</ul>
+<br>
+
+<hr>
+
+<p><b>Access Grid Retreat</b><br>June 9-11, 2004<br><u>Toronto, Canada</u><br>The 4th Access Grid (AG) Retreat will be hosted by Ryerson University in their Rogers Communication Center, an ideal venue for collaboration and interaction. <a href="http://www-unix.mcs.anl.gov/fl/flevents/ag/agr04/">More information</a>.</p>
+
+<p><b>GGF 10</b><br>March 9-13, 2004<br><u>Berlin, Germany</u><br>The Global Grid Forum's tenth meeting will feature four days of working group sessions and workshops. Details are at <a href="http://www.ggf.org/">http://www.ggf.org/</a>.</p>
+
+<p><b>CCGrid 2004</b><br> 
+April 19-22, 2004<br>
+<u>Chicago, IL</u><br> 
+CCGrid2004, sponsored by the IEEE Computer Society (final approval pending), is designed to bring 
+together international leaders who are pioneering researchers, developers, and users of extreme 
+clusters, networks, and Grid architectures and applications. The symposium will also serve as a 
+forum to present the latest work, and highlight related activities from around the world. 
+<a href="http://www.mcs.anl.gov/ccgrid2004">More information</a>.</p>
+
+<p><b>GlobusWORLD 2004</b><br>
+January 19-23, 2004<br>
+<u>San Francisco, California</u><br>
+GlobusWORLD 2004  featured three tracks of invited speakers, lecturers, interactive panels, and 
+forward-looking roundtables on Grid computing topics related to the Globus Toolkit. It followed 
+the successful first GlobusWORLD held in January 2003, with over 450 attendees from 25 
+countries. See <a href="http://www.globusworld.org">http://www.globusworld.org</a>.</p>
+
+<p><b>SC2003: Igniting Innovation</b><br>November 15-21, 2003<br><u>Phoenix, Arizona</u><br>The SC conference marks its 15th year with SC2003. Thousands of high-performance computing and networking experts will see the latest technological tools, learn about new scientific applications, and listen to other experts present their most recent research. See <a href="http://www.sc-conference.org/sc2003/">http://www.sc-conference.org/sc2003/</a>.</p>
+
+
+<p><b>GGF9</b>
+<br>October 5-8, 2003
+<br><u>Chicago, IL</u>
+<br>The Global Grid Forum's ninth meeting will feature four days of over 100 working group sessions and workshops.&nbsp; Details are at <a href="http://www.ggf.org/">http://www.ggf.org/</a>.</p>
+
+<p><b>Applications Grid Workshop</b>
+<br>September 9-10, 2003
+<br><u>Czestochowa, Poland </u>
+<br>The Applications Grid Workshop (AGW) will be held in conjunction with the Fifth International Conference on Parallel Processing and Applied Mathematics. AGW seeks to provide a bridge between the application community and the developers and directors of grid policies and infrastructures. The workshop results may be used by the national and European governments while building the global grid infrastructures. AGW's call for papers is at <a href="http://www.isi.edu/~deelman/cfp-agw.htm"> http://www.isi.edu/~deelman/cfp-agw.htm</a>.</p>
+
+
+<p><b><a href="http://www.gridforum.org">GGF8</a></b>
+<br>The Global Grid Forum met in Seattle on June 24-27, 2003. GGF8's theme is &quot;Building Grids -- Obstacles and Opportunities.&quot; Details are at <a href="http://www.gridforum.org/meetings/ggf8/"> http://www.gridforum.org/meetings/ggf8/</a>.</p>
+
+<p><a href="http://www.isi.edu/~deelman/cfp-agw.htm"><b>Applications Grid Workshop</b></a>
+<br>The Applications Grid Workshop (AGW) will be held September 9-10, 2003 in Czestochowa, Poland, in conjunction with the Fifth International Conference on Parallel Processing and Applied Mathematics. AGW seeks to provide a bridge between the application community and the developers and directors of grid policies and infrastructures. The workshop results may be used by the national and European governments while building the global grid infrastructures.</p>
+
+<p><b><a href="http://www.gridforum.org">GGF7</a></b>
+<br>The Global Grid Forum held its first meeting in the Asia/Pacific region
+March 4-7, 2003. GGF7's theme was &quot;Grids Around the World.&quot;</p>
+
+
+<p><b><a href="http://www.globusworld.org/">GlobusWorld</a></b>
+<br>The first GlobusWorld conference was held in San Diego, January 13-17, 2003. Designed for enterprise planners and Grid architects, administrators, and application developers, this conference  provided a wealth of information for anyone using or planning to use Grid computing in their science or business.</p>
+
+<p><b>SC2002</b>
+<br>The Globus Project was involved in many ways at SC2002 in Baltimore, 
+November 16-22, 2002. 
+
+
+<p><b>Edinburgh Tutorials</a></b>
+<br>The Globus Project, in conjunction with the U.K. eScience Institute, presented four days of tutorials in Edinburgh in January 2002.</p>
+    
+<p><b>Chicago Tutorials</b>
+<br>The Globus Project presented five days of tutorials at Argonne National Laboratory in Chicago, Illinois in late January 2002.</p>
+    
+<p><b>SC2001</b>
+<br>At SC2001 (November 10-16), the Globus Project showed recent work on Grid applications built using the Globus Toolkit, released the Globus Toolkit 2.0, and demonstrated our recent work on advanced Grid technologies.  During the conference, the Globus Toolkit was demonstrated in action by numerous application demonstrations around the exhibit hall.  The Globus Project was also represented by paper presentations, tutorials, posters, SC Global presentations and panels, and other conference activities.</p>
+    
+<p><b>Introduction to Grid Computing and the Globus Toolkit</b>
+<br> A practical introduction both to Grid computing, and the Globus Toolkit was held Friday, October 12, 9:30am - 5:30pm, at the e-Science Institute, 15 South College Street, Edinburgh, Scotland. </p>
+    
+<p><b><a href="http://www.informatica.unile.it/laboratori/lab-hpc/euroglobus/index.htm">EuroGlobus</a></b>
+<br> The first EuroGlobus conference was held June 16-23, 2001 in Lecce, Italy.</p>
+
+<p><b>Globus Retreat 2001</b>
+<br> The Globus Retreat was held in August in San Francisco. Like previous meetings in this series, this event was an opportunity for users and developers of the Globus Toolkit to discuss common interests, learn about recent developments, and plan future directions.</p>
+
+<p><b><a href="http://www-sop.inria.fr/JGI2001/">Joint ACM Java Grande/ISCOPE 2001 Conference</a></b>
+<br>This full day tutorial--introducing JavaGrande activities--included details on Java-based Grid programming using Globus services. The tutorial took place on June 2 in San Francisco during the Java One conference.</p>
+
+<p><b><a href="http://www.ippsxx.org/ipdps2001/2001_tutorial4.html">IPDPS 2001</a></b>
+<br>We provided a tutorial on grid computing, Globus, and Java interfaces to the Grid at IPDPD 2001 in San Francisco, April 27th, 2001.</p>
+
+<p><b>Supercomputing 2000</b>
+<br>Supercomputing 2000 was held on November 4-10, 2000 at the Dallas Convention Center. The Globus Project demonstrated our work on data grid technologies and applications that use these technologies.</p>
+
+<p><b><a href="http://www.isi.edu/info-services/">Information Services Workshop</a></b>
+<br>Our Information Services Workshop was held at NASA Ames Research Center on September 21-22, 2000. The workshop provided a forum for discussing current experiences and new approaches used to provide the necessary information to the Grid computing infrastructure. We discussed various issues pertaining to the discovery, acquisition and dissemination of Grid-related information. [<a href="http://www.isi.edu/info-services/agenda.html">slides</a>]</p>
+
+<p><b>Globus Retreat 2000</b>
+<br>The 2000 Globus Retreat was held July 30th through August 1st in Pittsburgh, PA. These were the three days before the IEEE High Performance Distributed Computing (HPDC) symposium. (See the <a href="http://www.cs.cmu.edu/~hpdc/">HPDC website</a>.) This retreat provided an opportunity for users and developers of the Globus Toolkit to discuss common interests. </p>
+
+<p><b>Globus Tutorials (U.K.)</b>
+<br>Rutherford Appleton Laboratory in the United Kingdom hosted a two-day Globus Tutorial on June 21-22, 2000. These tutorials covered basic grid concepts, Globus Toolkit components, and details for programmers using the Globus Toolkit.</p>
+
+<p><b>Fifth Globus Retreat: August 1-3, 1999, Redondo Beach, CA</b>
+<br>The fifth Globus retreat was held on August 1-3 in Redondo Beach, CA, the three days before the IEEE High Performance Distributed Computing symposium, which should also be of interest (see <a href="http://www.isi.edu/hpdc8/">http://www.isi.edu/hpdc8/</a>). These events provided an opportunity for users and developers of the Globus distributed computing toolkit to discuss common interests. <br>
+    The program included: 
+<ul>
+   <li>Tutorials on Globus programming and Globus system administration, on Sunday at ISI</li>
+   <li>Technical program on Monday and Tuesday, including talks by Globus users and updates on Globus status </li>
+   <li>Discussion of future directions </li>
+</ul>
+   
+ <p><b>Fourth Globus Retreat: August 2-4, 1998, Chicago</b>
+<br> The fourth Globus retreat was held August 2-4 in Chicago, the week after the IEEE High Performance Distributed Computing symposium (<a href="http://www.mcs.anl.gov/hpdc7">HPDC-7</a>), which should also be of interest. These events provide an opportunity for users and developers of the Globus distributed computing toolkit to discuss common interests.</p>
+<p>The program included
+<ul>
+      <li>Tutorials on Globus programming and Globus system administration, from 2-6 pm on Sunday August 2nd</li>
+      <li>Reception Sunday evening, 7-9 pm</li>
+      <li>Technical program on Monday and Tuesday, including talks by Globus users and updates on Globus status</li>
+      <li>Discussion of future directions</li>
+ </ul>
+    
+ 
+<p><b>December 4 &amp; 5, 1996, San Diego</b>
+<br>The second Globus retreat was held at the San Diego Supercomputer Center. The primary purpose of the meeting was to present and review basic Globus services and associated APIs (for which specifications are available), and the GUSTO testbed on which these services are being deployed. Status reports were also given on a number of related projects. Thanks a lot for the San Diego Supercomputer Center, and in particular to Charlotte Smart and Reagan Moore, for hosting the meeting.</p>
+    <p>The following is a summary of the schedule; </p>
+<ul>
+      <li>Globus project overview and Globus model</a></li>
+      <li>Communication API presentation and review (Steve Tuecke)</li>
+      <li>Security API presentation and review (Carl Kesselman)</li>
+      <li>Information service API presentation and review (Carl Kesselman)</li>
+      <li>Data access API presentation and review (Ian Foster)</li>
+      <li>GUSTO testbed design> (Ian Foster)</li>
+      <li>Location/allocation API presentation and review (Warren Smith)</li>
+      <li>NWS and AppLeS project (Fran Berman and Rich Wolski)</li>
+      <li>DOCT project (Reagan Moore)</li>
+      <li>Condor project (Miron Livny)</li>
+      <li>NEOS project (Jorge More)</li>
+      <li>On the merits of computing stunts (Bill Nickless)</li>
+      <li>Strategy and planning</li>
+</ul>
+
+  
+
+<p><b>April 3 &amp; 4, 1996, Los Angeles</b>
+<br>The first Globus retreat was held at UCLA.</p>
+
+
+</div>
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
---
+++ alliance/events/index.php
@@ -0,0 +1,63 @@
+<?php
+
+$title = "Globus: Alliance - Meetings and Events";
+
+$section = "section-2";
+include_once( "../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+<!--
+<div id="left">
+<?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+</div>
+-->
+
+<div id="main">
+
+<h1 class="first">Meetings and Events</h1>
+
+<p>The Globus Alliance was or will be represented at the following meetings and 
+events.  The <a href="archive.php">Meetings and Events Archive</a> contains 
+information and notes from previous events.</p>
+    
+<p><b>2 Day Programming Tutorial with WSRF and the Globus Toolkit Version 4</b><br>
+May 3-4, 2005<br>
+<u>San Diego, CA</u><br>
+These two days of tutorials provide an in-depth introduction to programming with the 
+latest version of the Globus Toolkit (GT4).  The course is geared toward developers 
+who want to learn about the newest work of the Globus Alliance and how to apply 
+fundamental concepts in Grid computing.  The first day teaches developers how to 
+build a Java Service that makes use of GT4 mechanisms for state management, security, 
+registry, and related topics.  The second day is less hands on and more 
+presentation-oriented. It provides a comprehensive introduction to GT4, covering 
+execution management services, data movement and management services, and security services, and also 
+reviewing some of the higher-level tools that build on Globus mechanisms. A brief 
+introduction to administering a GT4-based Grid is also provided. To register,
+send email to <a href="mailto:gt4tutorial@novgp.com">gt4tutorial@novgp.com</a></p>
+
+<p><b>UK Globus Week, 2005</b><br>
+April 4-8, 2005<br>
+<u>Edinburgh, UK</u><br>
+UK Globus Week has 5 aims: to bring together people from the UK, Europe, and 
+globally with a significant interest in the Grid; to present Globus developments and 
+research; to provide a program of tutorials, workshops, Q&A, and demonstrations from 
+the Globus Alliance; to develops a roadmap for continued Globus activities with
+significant contributions from UK and European experiences and expertise; to document 
+the event's proceedings as a UK eScience Technical Report.
+
+</p>
+
+<p><b><a href="http://www.globusworld.org/">GlobusWORLD 2005</a></b>
+<br>February 7-11, 2005
+<br><u>Boston, Mass. </u>
+<br>GlobusWORLD is the premier conference on Grid computing, to be held in Boston, 
+February 7-11, 2005. The conference features parallel plenary sessions of Globus 
+Toolkit and Grid-related content for attendees from both industry and science. 
+<a class="learnmore" href="http://www.globusworld.org">Learn more...</a></p>
+
+<p>The <a href="archive.php">Meetings and Events Archive</a> contains 
+information and notes from previous events.</p>
+
+</div>
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
