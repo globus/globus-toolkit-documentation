aa582e0a78f5d740002b944534e7fd1f338b981a
liming <liming>
2005-04-05 21:27:50 +0000

    New look! and some updates

--- alliance/news/kit.php
+++ alliance/news/kit.php
@@ -6,22 +6,21 @@ include_once( "../../include/local.inc" );
 include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
 ?>
 
-<div id="left">
+<!--<div id="left">
 <?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
 </div>
+-->
 
 <div id="main">
 
-<!-- content STARTS here -->
-<p><< <a href="index.php">News </a> </p> 
-
-<h1>Globus Alliance Press Kit</h1>
+<h1 class="first">Globus Alliance Press Kit</h1>
     
 <p>The Globus Alliance is a research and software development project led 
 by Ian Foster (Argonne National Laboratory, University of Chicago) and 
 Carl Kesselman (University of Southern California Information Sciences 
-Institute).
-The goal of the Globus Alliance is to make Grid computing (with all the 
+Institute), with additional leadership from the Globus Alliance Board.</p>
+
+<p>The goal of the Globus Alliance is to make Grid computing (with all the 
 benefits that it provides) a realistic possibility in science, 
 engineering, business, and other situations where collaborative work 
 happens.</p>
@@ -30,36 +29,35 @@ happens.</p>
 information about the Globus Alliance for use by members of the press.
 </p>
 
-<p><b>Getting Started</b></p>
+<h2>Getting Started</h2>
 
 <p>For basic information about the Globus Alliance, the Globus Toolkit, 
 and Grid computing, it is probably easiest to start with our 
-<a href="http://www-unix.globus.org/r3/alliance/faq.php"> 
+<a href="<?=$SITE_PATHS["WEB_ROOT"]."faq.php"; ?>"> 
 Frequently-Asked Questions (FAQ)</a>.</p>
 
   
 <p>For an overview of Grid computing, a good reference is 
-<a href="../activities/papers/chapter2.pdf"> 
-Chapter 2 ("Computational Grids")</a>
-from the book <i>
-<!--<a href="http://www.globus.org/research/books.html">-->
-The Grid: Blueprint for a New Computing Infrastructure</i> by
-Ian Foster and Carl Kesselman, published by Morgan-Kaufman in 1999.
-</p>
+<a href="../publications/papers/chapter2.pdf">Chapter 2 ("Computational Grids")</a>
+from the book <i>The Grid: Blueprint for a New Computing Infrastructure</i> by
+Ian Foster and Carl Kesselman, published by Morgan-Kaufman in 1999. Another 
+high-level treatment of the subject can be found in the following article.</p>
+
+<blockquote><b>The Grid: A New Infrastructure for 21st Century Science</b>.
+I. Foster. <i>Physics Today</i>, 55(2):42-47, 2002.</blockquote>
 
 <p>For a more detailed overview of the Grid, including specific 
-architectural frameworks and a proposed Web Services framework for 
-Grid software (now referred to as the Open Grid Services Architecture, 
-or OGSA), you could review the following two papers.</p>
+architectural frameworks and the Open Grid Services Architecture (OGSA), please
+review the following two papers.</p>
 
 <blockquote><b>
-<a href="../activities/papers/anatomy.pdf">
+<a href="../publications/papers/anatomy.pdf">
 The Anatomy of the Grid: Enabling Scalable Virtual Organizations.</a></b>
 I. Foster, C. Kesselman, S. Tuecke. <i> International J. Supercomputer
 Applications</i>, 15(3), 2001.</blockquote>
 
 <blockquote><b>
-<a href="../activites/papers/ogsa.pdf">
+<a href="../publications/papers/ogsa.pdf">
 The Physiology of the Grid: An Open Grid Services Architecture for
 Distributed Systems Integration.</a></b> I. Foster, C. Kesselman, J. Nick, S.
 Tuecke; January, 2002.</blockquote>
@@ -70,7 +68,8 @@ Tuecke; January, 2002.</blockquote>
 provides a guide to the terminology and acronyms used.</p>
 -->
 
-<p><b>Contacts</b></p>
+<h2>Contacts</h2>
+
 <table>
 <tr><td><a href="http://www.mcs.anl.gov/people/foster">Ian Foster</a><br>
         Mathematics and Computer Science<br>
@@ -91,9 +90,7 @@ provides a guide to the terminology and acronyms used.</p>
     </table>
 
 
-<p></p>
-
-<p><b>Detailed Information</b></p>
+<h2>Detailed Information</h2>
 
 <p>This website itself is the most comprehensive collection of information
  about the Globus Project and the Globus Toolkit.  The various sections of
@@ -101,7 +98,6 @@ provides a guide to the terminology and acronyms used.</p>
 Toolkit to the details of our Application Programmer Interfaces (APIs), 
 and much in between.</p>
 
-<!-- content ENDS here -->
-
 </div>
+
 <?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
