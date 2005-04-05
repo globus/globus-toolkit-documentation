53d9b621b9966a10aa0fe7e49147014c742b0903
liming <liming>
2005-04-05 21:45:20 +0000

    Moved from activities

---
+++ alliance/publications/index.php
@@ -0,0 +1,32 @@
+<?php
+
+$title = "Globus: Alliance - Publications";
+
+$section = "section-2";
+include_once( "../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+<!--
+<div id="left">
+<?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+</div>
+-->
+
+<div id="home">
+
+<h1 class="first">Globus<sup><small>&reg;</small></sup> Publications</h1>
+
+<p>The Globus Alliance has produced an enormous amount of written material
+on the Grid, distributed computing, applied computer science, and related 
+topics.</p>
+
+<ul>
+<li><a href="papers.php">Research Papers</a></li>
+<li>Mass Media</li>
+<li>Presentations</li>
+<li>Globus Toolkit Documentation</li>
+</ul>
+
+</div>
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
