54d256f1045d086ab9e7aec9263a1fea70deb65f
liming <liming>
2005-04-06 16:41:34 +0000

    First edition

---
+++ alliance/publications/books/index.php
@@ -0,0 +1,98 @@
+<?php
+
+$title = "Globus: Publications - Books";
+
+$section = "section-2";
+include_once( "../../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+<!--
+<div id="left">
+<?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+</div>
+-->
+
+<div id="home">
+
+<h1 class="first">Globus<sup><small>&reg;</small></sup> Books</h1>
+
+<p>The following books were written by Globus Alliance members.</p>
+
+<table>
+  <tr>
+    <td><center><a href="http://www.amazon.com/exec/obidos/tg/detail/-/1558609334/qid=1112803437/sr=8-1/ref=pd_csp_1/103-4918058-7991028?v=glance&s=books&n=507846"><img border=0 src="thegrid2.jpg" alt="The Grid, 2nd Edition"></a></center></td>
+    <td><b><a href="http://www.amazon.com/exec/obidos/tg/detail/-/1558609334/qid=1112803437/sr=8-1/ref=pd_csp_1/103-4918058-7991028?v=glance&s=books&n=507846">The 
+           Grid 2: Blueprint for a New Computing Infrastructure</a></b><br>
+        Edited by Ian Foster and Carl Kesselman<br>
+        Morgan Kaufmann; 2 edition (November 18, 2003)<br>
+        ISBN: 1558609334
+        <p>In 1998, Ian Foster and Carl Kesselman introduced a whole 
+           new concept in computing with the first edition of this book. 
+           Today there is a broader and deeper understanding of the nature 
+           of the opportunities offered by Grid computing and the 
+           technologies needed to realize those opportunities. In Grid 2, 
+           the editors reveal the revolutionary impact of large-scale 
+           resource sharing and virtualization within science and industry, 
+           the intimate relationships between organization and resource 
+           sharing structures and the new technologies required to enable 
+           secure, reliable, and efficient resource sharing on large scale.</p>
+           <p>Foster and Kesselman have once again assembled a team of 
+           experts to present an up-to-date view of Grids that reports on 
+           real experiences and explains the available technologies and new 
+           technologies emerging from labs, companies and standards bodies. 
+           Grid 2, like its predecessor, serves as a manifesto, design 
+           blueprint, user guide and research agenda for future Grid systems.</p>
+        </p>
+    </td>
+  </tr>
+  <tr>
+    <td><center><a href=""><img border=0 src="resmgmt.jpg" alt="Grid Resource Management"></a></center></td>
+    <td><b><a href="">Grid 
+           Resource Management: State of the Art and Future Trends</a></b><br>
+        Edited by Jarek Nabrzyski, Jennifer M. Schopf, Jan Weglarz<br>
+        Springer; 1 edition (September, 2003)<br>
+        ISBN: 1402075758
+        <p>Grid Resource Management: State of the Art and Future Trends 
+           presents an overview of the state of the field and describes 
+           both the real experiences and the current research available 
+           today. Grid computing is a rapidly developing and changing 
+           field, involving the shared and coordinated use of dynamic, 
+           multi-institutional resources. Grid resource management is 
+           the process of identifying requirements, matching resources 
+           to applications, allocating those resources, and scheduling 
+           and monitoring Grid resources over time in order to run Grid 
+           applications as efficiently as possible. While Grids have 
+           become almost commonplace, the use of good Grid resource 
+           management tools is far from ubiquitous because of the many 
+           open issues of the field, including the multiple layers of 
+           schedulers, the lack of control over resources, the fact that 
+           resources are shared, and that users and administrators have 
+           conflicting performance goals. These are the issues addressed 
+           in this book, in addition to elucidating the overlap with 
+           related areas including discussions of work with peer-to-peer 
+           computing, economic approaches, and operations research. Grid 
+           Resource Management: State of the Art and Future Trends is 
+           an invaluable resource for today's user, application 
+           developer, or resource owners when working with Grid resource 
+           management systems.</p>
+    </td>
+  </tr>
+  <tr>
+    <td><center><a href="http://www.amazon.com/exec/obidos/tg/detail/-/0201575949/qid=1112803437/sr=8-2/ref=pd_ka_1/103-4918058-7991028?v=glance&s=books&n=507846"><img border=0 src="parallelprogs.jpg" alt="Designing & Building Parallel Programs"></a></center></td>
+    <td><b><a href="http://www.amazon.com/exec/obidos/tg/detail/-/0201575949/qid=1112803437/sr=8-2/ref=pd_ka_1/103-4918058-7991028?v=glance&s=books&n=507846">Designing 
+           and Building Parallel Programs: Concepts and 
+           Tools for Parallel Software Engineering</a></b><br>
+        Ian Foster<br>
+        Addison Wesley; 1st edition (January 31, 1995)<br>
+        ISBN: 0201575949
+        <p>Provides a comprehensive introduction to parallel algorithm 
+           design, performance analysis, and program construction. 
+           Describes the tools needed to write parallel programs and 
+           provides numerous examples.</p>
+    </td>
+  </tr>
+</table>
+
+</div>
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
