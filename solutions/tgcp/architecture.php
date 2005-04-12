09f9458f9d5295a8fd2537fa4329794bf9005448
liming <liming>
2005-04-12 19:15:44 +0000

    Empty page

---
+++ solutions/tgcp/architecture.php
@@ -0,0 +1,20 @@
+<?php
+$title = "Grid Solutions - The TeraGrid Copy Solution (Architecture)";
+$section = "section-5";
+include_once( "../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+include_once($SITE_PATHS["SERV_INC"] . "app-info.inc");
+?>
+<!--<div id="left">
+<?php include($SITE_PATHS["SERV_INC"].'solutions.inc'); ?>
+</div>
+-->
+
+<div id="main">
+
+<h1 class="first">TeraGrid Copy Architecture</h1>
+
+
+</div>
+
+<? include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
---
+++ solutions/tgcp/deployment.php
@@ -0,0 +1,20 @@
+<?php
+$title = "Grid Solutions - The TeraGrid Copy Solution (Deployment Details)";
+$section = "section-5";
+include_once( "../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+include_once($SITE_PATHS["SERV_INC"] . "app-info.inc");
+?>
+<!--<div id="left">
+<?php include($SITE_PATHS["SERV_INC"].'solutions.inc'); ?>
+</div>
+-->
+
+<div id="main">
+
+<h1 class="first">TeraGrid Copy Deployment</h1>
+
+
+</div>
+
+<? include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
