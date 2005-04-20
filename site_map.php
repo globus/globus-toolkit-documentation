e97fb679e61f491925412ccf41fed36b048b9cc1
liming <liming>
2005-04-20 21:03:09 +0000

    First draft, no links

---
+++ site_map.php
@@ -0,0 +1,188 @@
+<?php
+$title = "Globus: Site Map";
+$section = "section-1";
+include_once( "include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
+?>
+
+<div id="main">
+
+ <h1 class="first">Site Map</h1>
+   
+ <p>This page list all of the major sections of the Globus website and their contents.
+    There is also a <a href="site_index.php">Site Index</a> page that is 
+    organized in alphabetical order.  Please send suggestions for additions or 
+    corrections to <a href="mailto:webmaster@globus.org">webmaster@globus.org</a>.</p>
+
+ <p><b>Links available on all pages:</b>
+ Contact Us |
+ FAQ |
+ Home |
+ Site Index |
+ Site Map
+ </p>
+ 
+ <h2>Globus Alliance</h2>
+
+<p>About the Globus Alliance</p>
+
+<p><b>People and Organizations:</b>
+Academic Affiliates |
+e-Science/e-Business Projects |
+Globus Alumni | 
+Globus Team | 
+Job Opportunities |
+Sponsors
+</p>
+
+<p><b>Publications:</b>
+Books |
+Globus Toolkit Documentation |
+Research Papers
+</p>
+
+<p><b>News and Events:</b>
+Impact of Our Work |
+Meetings and Events | 
+Meetings and Events Archive |
+News Archive | 
+Press Kit
+</p>
+
+ <h2>Globus Toolkit</h2>
+
+<p>About the Globus Toolkit | Archive of Globus Toolkit News</p>
+
+<p><b>About:</b>
+In the Press |
+How do I cite Globus? |
+Success Stories |
+What is the Globus Toolkit?
+</p>
+
+<p><b>Who's Involved:</b>
+Contributors |
+Globus Toolkit Team |
+How can I contribute to the Globus Toolkit? |
+Sponsors
+</p>
+
+<p><b>Downloads:</b>
+Advisories |
+Developer tools |
+Development Releases |
+Latest Stable Release: 4.0 |
+Quality Assurance |
+Related Software |
+Software Archive
+</p>
+
+<p><b>Documentation:</b>
+Development Documentation |
+Documentation Archive |
+Globus Community Documentation |
+Latest Stable Release: 4.0 |
+Presentations & Tutorials
+</p>
+
+<p><b>Support:</b>
+Bugzilla |
+Mailing lists |
+Support overview |
+Training
+</p>
+
+<p><b>Components:</b>
+Common Runtime Components |
+Data Management |
+Execution Management |
+Information Services |
+Security
+</p>
+
+ <h2>Grid Software</h2>
+
+<p>
+An Ecosystem of Grid Components |
+Role of the Globus Toolkit
+</p>
+
+<p><b>Security:</b>
+Community Authorization Service (CAS) |
+Globus Certificate Service |
+KX.509 and KCA |
+MyProxy |
+PKINIT | 
+Pre-WS Authentication and Authorization | 
+Shibboleth |
+Simple CA |
+VOMS |
+Web Services Authentication and Authorization
+</p>
+
+<p><b>Monitoring and Discovery:</b>
+Ganglia Cluster Toolkit |
+Globus Index Service |
+Globus Trigger Service |
+Inca |
+MonALISA |
+WS Core Monitoring Features
+</p>
+
+<p><b>Computation:</b>
+Chimera |
+Condor-G, DAGman |
+GRAM |
+GRAM Scheduler Plugins |
+MPICH-G2 | 
+NetSolve/GridSolve | 
+Ninf-G | 
+Platform CSF
+</p>
+
+<p><b>Data:</b>
+Chimera |
+DataCutter |
+globus-url-copy |
+GridFTP |
+GSI-SCP/SFTP |
+Metadata Catalog Service (MCS) |
+NeST |
+OGSA-DAI |
+Reliable File Transfer (RFT) Service |
+Replica Location Service (RLS) |
+UberFTP
+</p>
+
+<p><b>Collaboration:</b>
+Access Grid |
+Open Grid Collaboration Environment (OGCE) |
+Sakai
+</p>
+
+<p><b>Packaging and Distribution:</b>
+Grid Packaging Tools (GPT) |
+NSF Midddleware Initiative (NMI) |
+PACMAN |
+Rocks |
+Virtual Data Toolkit (VDT)
+</p>
+
+ <h2>Grid Solutions</h2>
+
+<p><b>Security Challenges:</b>
+Registering Users For The Earth System Grid
+</p>
+
+<p><b>Monitoring/Managability Challenges:</b>
+A Monitoring System For The Earth System Grid
+</p>
+
+<p><b>Data Challenges:</b>
+Balancing Computation And Storage For SDSS |
+Data Replication For LIGO |
+Moving Data Fast On The TeraGrid
+</p>
+
+</div>
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
