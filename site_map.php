90e47575d5318a1d0531bc1db8b143db3c855d1e
liming <liming>
2005-04-21 00:49:12 +0000

    Added all hyperlinks

--- site_map.php
+++ site_map.php
@@ -14,174 +14,184 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
     organized in alphabetical order.  Please send suggestions for additions or 
     corrections to <a href="mailto:webmaster@globus.org">webmaster@globus.org</a>.</p>
 
- <p><b>Links available on all pages:</b>
- Contact Us |
- FAQ |
- Home |
- Site Index |
- Site Map
+ <p><b>Links available on all pages:</b><br>
+ <a href="<?=$SITE_PATHS["WEB_ROOT"]."contacts.php"; ?>">Contact Us</a> |
+ <a href="<?=$SITE_PATHS["WEB_ROOT"]."faq.php"; ?>">FAQ</a> |
+ <a href="<?=$SITE_PATHS["WEB_ROOT"]; ?>">Home</a> |
+ <a href="<?=$SITE_PATHS["WEB_ROOT"]."site_index.php"; ?>">Site Index</a> |
+ <a href="<?=$SITE_PATHS["WEB_ROOT"]."site_map.php"; ?>">Site Map</a>
  </p>
  
- <h2>Globus Alliance</h2>
+<hr>
 
-<p>About the Globus Alliance</p>
+<h2><a href="<?=$SITE_PATHS["WEB_ALLIANCE"]; ?>">Globus Alliance</a></h2>
 
-<p><b>People and Organizations:</b>
-Academic Affiliates |
-e-Science/e-Business Projects |
-Globus Alumni | 
-Globus Team | 
-Job Opportunities |
-Sponsors
+<p><a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."about.php"; ?>">About the Globus Alliance</a></p>
+
+<p><b>People and Organizations:</b><br>
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."affiliates.php"; ?>">Academic Affiliates</a> |
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."projects.php"; ?>">e-Science/e-Business Projects</a> |
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."team/alumni.php"; ?>">Globus Alumni</a> | 
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."team/"; ?>">Globus Team</a> | 
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."employment/"; ?>">Job Opportunities</a> |
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."sponsors.php"; ?>">Sponsors</a>
 </p>
 
-<p><b>Publications:</b>
-Books |
-Globus Toolkit Documentation |
-Research Papers
+<p><b>Publications:</b><br>
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."publications/books/"; ?>">Books</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."docs/"; ?>">Globus Toolkit Documentation</a> |
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."publications/papers.php"; ?>">Research Papers</a>
 </p>
 
-<p><b>News and Events:</b>
-Impact of Our Work |
-Meetings and Events | 
-Meetings and Events Archive |
-News Archive | 
-Press Kit
+<p><b>News and Events:</b><br>
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."impact/"; ?>">Impact of Our Work</a> |
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."events/"; ?>">Meetings and Events</a> | 
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."events/archive.php"; ?>">Meetings and Events Archive</a> |
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."news/"; ?>">News Archive</a> | 
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."news/kit.php"; ?>">Press Kit</a>
 </p>
 
- <h2>Globus Toolkit</h2>
+<hr>
 
-<p>About the Globus Toolkit | Archive of Globus Toolkit News</p>
+<h2><a href="<?=$SITE_PATHS["WEB_TOOLKIT"]; ?>">Globus Toolkit</a></h2>
 
-<p><b>About:</b>
-In the Press |
-How do I cite Globus? |
-Success Stories |
-What is the Globus Toolkit?
+<p><a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."about.html"; ?>">About the Globus Toolkit</a> | 
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."news.html"; ?>">Archive of Globus Toolkit News</a>
 </p>
 
-<p><b>Who's Involved:</b>
-Contributors |
-Globus Toolkit Team |
-How can I contribute to the Globus Toolkit? |
-Sponsors
+<p><b>About:</b><br>
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."press.html"; ?>">In the Press</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."citations.html"; ?>">How do I cite Globus?</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."success_stories.html"; ?>">Success Stories</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."about.html"; ?>">What is the Globus Toolkit?</a>
 </p>
 
-<p><b>Downloads:</b>
-Advisories |
-Developer tools |
-Development Releases |
-Latest Stable Release: 4.0 |
-Quality Assurance |
-Related Software |
-Software Archive
+<p><b>Who's Involved:</b><br>
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."contributors.html"; ?>">Contributors</a> |
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."team/"; ?>">Globus Toolkit Team</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."contributions.html"; ?>">How can I contribute to the Globus Toolkit?</a> |
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."sponsors.php"; ?>">Sponsors</a>
 </p>
 
-<p><b>Documentation:</b>
-Development Documentation |
-Documentation Archive |
-Globus Community Documentation |
-Latest Stable Release: 4.0 |
-Presentations & Tutorials
+<p><b>Downloads:</b><br>
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."advisories.html"; ?>">Advisories</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."dev_tools.html"; ?>">Developer tools</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."downloads/development/"; ?>">Development Releases</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."downloads/4.0.0/"; ?>">Latest Stable Release: 4.0</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."quality_assurance.html"; ?>">Quality Assurance</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."tools/"; ?>">Related Software</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."downloads/"; ?>">Software Archive</a>
 </p>
 
-<p><b>Support:</b>
-Bugzilla |
-Mailing lists |
-Support overview |
-Training
+<p><b>Documentation:</b><br>
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."docs/development/"; ?>">Development Documentation</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."docs/"; ?>">Documentation Archive</a> |
+<a href="http://gdp.globus.org/">Globus Community Documentation</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."docs/4.0/"; ?>">Latest Stable Release: 4.0</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."presentations/"; ?>">Presentations & Tutorials</a>
 </p>
 
-<p><b>Components:</b>
-Common Runtime Components |
-Data Management |
-Execution Management |
-Information Services |
-Security
+<p><b>Support:</b><br>
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."bugzilla.html"; ?>">Bugzilla</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."support.html#mailinglists"; ?>">Mailing lists</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."support.html"; ?>">Support overview</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."support.html#training"; ?>">Training</a>
 </p>
 
- <h2>Grid Software</h2>
+<p><b>Components:</b><br>
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"].""; ?>">Common Runtime Components</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."data/"; ?>">Data Management</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"].""; ?>">Execution Management</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."mds/"; ?>">Information Services</a> |
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."security/"; ?>">Security</a>
+</p>
+
+<hr>
+
+<h2><a href="<?=$SITE_PATHS["WEB_SOFTWARE"]; ?>">Grid Software</a></h2>
 
 <p>
-An Ecosystem of Grid Components |
-Role of the Globus Toolkit
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."ecology.php"; ?>">An Ecosystem of Grid Components</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."role-of-gt.php"; ?>">Role of the Globus Toolkit</a>
 </p>
 
-<p><b>Security:</b>
-Community Authorization Service (CAS) |
-Globus Certificate Service |
-KX.509 and KCA |
-MyProxy |
-PKINIT | 
-Pre-WS Authentication and Authorization | 
-Shibboleth |
-Simple CA |
-VOMS |
-Web Services Authentication and Authorization
+<p><b><a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/"; ?>">Security</a>:</b><br>
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/cas.php"; ?>">Community Authorization Service (CAS)</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/globus-cert-service.php"; ?>">Globus Certificate Service</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/kx509-and-kca.php"; ?>">KX.509 and KCA</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/myproxy.php"; ?>">MyProxy</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/pkinit.php"; ?>">PKINIT</a> | 
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/pre-ws-aa.php"; ?>">Pre-WS Authentication and Authorization</a> | 
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/shibboleth.php"; ?>">Shibboleth</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/simple-ca.php"; ?>">Simple CA</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/voms.php"; ?>">VOMS</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."security/ws-aa.php"; ?>">Web Services Authentication and Authorization</a>
 </p>
 
-<p><b>Monitoring and Discovery:</b>
-Ganglia Cluster Toolkit |
-Globus Index Service |
-Globus Trigger Service |
-Inca |
-MonALISA |
-WS Core Monitoring Features
+<p><b><a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."monitoring/"; ?>">Monitoring and Discovery</a>:</b><br>
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."monitoring/ganglia.php"; ?>">Ganglia Cluster Toolkit</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."monitoring/index-service.php"; ?>">Globus Index Service</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."monitoring/trigger-service.php"; ?>">Globus Trigger Service</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."monitoring/inca.php"; ?>">Inca</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."monitoring/monalisa.php"; ?>">MonALISA</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."monitoring/ws-core.php"; ?>">WS Core Monitoring Features</a>
 </p>
 
-<p><b>Computation:</b>
-Chimera |
-Condor-G, DAGman |
-GRAM |
-GRAM Scheduler Plugins |
-MPICH-G2 | 
-NetSolve/GridSolve | 
-Ninf-G | 
-Platform CSF
+<p><b><a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/"; ?>">Computation</a>:</b><br>
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/chimera.php"; ?>">Chimera</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/condor-g.php"; ?>">Condor-G, DAGman</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/gram.php"; ?>">GRAM</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/gram-plugins.php"; ?>">GRAM Scheduler Plugins</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/mpich-g2.php"; ?>">MPICH-G2</a> | 
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/netsolve.php"; ?>">NetSolve/GridSolve</a> | 
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/ninf-g.php"; ?>">Ninf-G</a> | 
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/csf.php"; ?>">Platform CSF</a>
 </p>
 
-<p><b>Data:</b>
-Chimera |
-DataCutter |
-globus-url-copy |
-GridFTP |
-GSI-SCP/SFTP |
-Metadata Catalog Service (MCS) |
-NeST |
-OGSA-DAI |
-Reliable File Transfer (RFT) Service |
-Replica Location Service (RLS) |
-UberFTP
+<p><b><a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/"; ?>">Data</a>:</b><br>
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."computation/chimera.php"; ?>">Chimera</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/datacutter.php"; ?>">DataCutter</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/globus-url-copy.php"; ?>">globus-url-copy</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/gridftp.php"; ?>">GridFTP</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/scp.php"; ?>">GSI-SCP/SFTP</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/mcs.php"; ?>">Metadata Catalog Service (MCS)</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/nest.php"; ?>">NeST</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/dai.php"; ?>">OGSA-DAI</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/rft.php"; ?>">Reliable File Transfer (RFT) Service</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/rls.php"; ?>">Replica Location Service (RLS)</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."data/uberftp.php"; ?>">UberFTP</a>
 </p>
 
-<p><b>Collaboration:</b>
-Access Grid |
-Open Grid Collaboration Environment (OGCE) |
-Sakai
+<p><b><a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."collaboration/"; ?>">Collaboration</a>:</b><br>
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."collaboration/accessgrid.php"; ?>">Access Grid</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."collaboration/ogce.php"; ?>">Open Grid Collaboration Environment (OGCE)</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."collaboration/chef.php"; ?>">Sakai</a>
 </p>
 
-<p><b>Packaging and Distribution:</b>
-Grid Packaging Tools (GPT) |
-NSF Midddleware Initiative (NMI) |
-PACMAN |
-Rocks |
-Virtual Data Toolkit (VDT)
+<p><b><a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."packaging/"; ?>">Packaging and Distribution</a>:</b><br>
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."packaging/gpt.php"; ?>">Grid Packaging Tools (GPT)</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."packaging/nmi.php"; ?>">NSF Midddleware Initiative (NMI)</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."packaging/pacman.php"; ?>">PACMAN</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."packaging/rocks.php"; ?>">Rocks</a> |
+<a href="<?=$SITE_PATHS["WEB_SOFTWARE"]."packaging/vdt.php"; ?>">Virtual Data Toolkit (VDT)</a>
 </p>
 
- <h2>Grid Solutions</h2>
+<hr>
 
-<p><b>Security Challenges:</b>
-Registering Users For The Earth System Grid
+ <h2><a href="<?=$SITE_PATHS["WEB_SOLUTIONS"]; ?>">Grid Solutions</a></h2>
+
+<p><b>Security Challenges:</b><br>
+<a href="<?=$SITE_PATHS["WEB_SOLUTIONS"]; ?>">Registering Users For The Earth System Grid</a>
 </p>
 
-<p><b>Monitoring/Managability Challenges:</b>
-A Monitoring System For The Earth System Grid
+<p><b>Monitoring/Managability Challenges:</b><br>
+<a href="<?=$SITE_PATHS["WEB_SOLUTIONS"]."system_monitoring/"; ?>">A Monitoring System For The Earth System Grid</a>
 </p>
 
-<p><b>Data Challenges:</b>
-Balancing Computation And Storage For SDSS |
-Data Replication For LIGO |
-Moving Data Fast On The TeraGrid
+<p><b>Data Challenges:</b><br>
+<a href="<?=$SITE_PATHS["WEB_SOLUTIONS"]; ?>">Balancing Computation And Storage For SDSS</a> |
+<a href="<?=$SITE_PATHS["WEB_SOLUTIONS"]."data_replication/"; ?>">Data Replication For LIGO</a> |
+<a href="<?=$SITE_PATHS["WEB_SOLUTIONS"]."tgcp/"; ?>">Moving Data Fast On The TeraGrid</a>
 </p>
 
 </div>
