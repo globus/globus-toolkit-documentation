6494fee3a3d417eb1195a6330cb04d9520be951c
liming <liming>
2005-05-04 04:55:27 +0000

    Added per request by Jennifer Schopf

---
+++ mds/index.php
@@ -0,0 +1,30 @@
+<?php
+$title = "Globus: Monitoring and Discovery System";
+$section = "section-1";
+include_once( "../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+
+<div id="main">
+ 
+  <h1 class="first">MDS - Monitoring and Discovery System</h1>
+
+  <p>In conjunction with the release of the Globus Toolkit 4.0, we have 
+     restructured our website to improve usability. The information
+     formerly at this location has been moved, and this address will be
+     phased out soon.  Please use the following address to reach the new
+     location.</p>
+
+  <p><a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."mds/"; ?>">http://www.globus.org/toolkit/mds/</a></p>
+
+  <p>You may also find the 
+     <a href="<?=$SITE_PATHS["WEB_ROOT"]."site_map.php"; ?>">Site Map</a> 
+     useful for locating other information on the new website.</p>
+
+  <p>We realize this change is inconvenient. It is our hope that the
+     inconvenience will be outweighed by the new site's improved 
+     usability.</p>
+
+</div>
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
