f9f74235f8b13e4310f56a77834c9b3d458ab2ee
liming <liming>
2005-05-13 15:46:18 +0000

    Added research papers link

--- 404.php
+++ 404.php
@@ -17,15 +17,17 @@ you are looking for:
 </p>
 
 <ul>
-  <li>Use the navigation bar at the top of the page.
-  <li>Use the new <a href="<?=$SITE_PATHS["WEB_ROOT"]."site_map.php"; ?>">site map</a>.
-  <li>Use the site search.
+  <li>Use the navigation bar at the top of the page.</li>
+  <li>Use the new <a href="<?=$SITE_PATHS["WEB_ROOT"]."site_map.php"; ?>">site map</a>.</li>
+  <li>Use the site search.</li>
   <li>If you got the error by navigating the Globus website and think
       that the link is broken, please report it to
       <a href="mailto:webmaster@globus.org">webmaster@globus.org</a>.
-  <li>If you are looking for GT4 documentation (3.9.x or 4.0-drafts),
+  <li>If you are looking for <b>GT4 documentation</b> (3.9.x or 4.0-drafts),
       please go to 
-      <a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."docs/4.0/"; ?>">http://www.globus.org/toolkit/docs/4.0/</a>.
+      <a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."docs/4.0/"; ?>">http://www.globus.org/toolkit/docs/4.0/</a>.</li>
+  <li>If you are looking for our <b>research papers</b>, please go to
+      <a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."publications/papers.php"; ?>">http://www.globus.org/alliance/publications/papers.php</a>.</li>
 </ul>
 
 </div>
