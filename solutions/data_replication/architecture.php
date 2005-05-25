96e8cf442a69b859ffb32a8190301fe510691e02
liming <liming>
2005-05-25 20:14:07 +0000

    Replaced with content from Koranda

--- solutions/data_replication/architecture.php
+++ solutions/data_replication/architecture.php
@@ -12,149 +12,37 @@ include($SITE_PATHS["SERV_INC"] . "app-info.inc");
 
 <div id="main">
 
-<h1 class="first">Lightweight Data Replicator System Architecture</h1>
+<h1 class="first">Constructing the Lightweight Data Replicator</h1>
 
-<p>
-<b>NOTE:</b> The design description below was copied verbatim from the 
-<a href="http://www.lsc-group.phys.uwm.edu/LDR/design.html">Lightweight Data Replicator (LDR)</a>
-website. It was written by the LDR developers.
-</p>
+<p>A Data Grid is a set of geographically distributed sites, institutions, or groups that have a common need to store and access specific data items. In order to provide reasonable performance to users, the Data Grid may need to replicate data items at several sites. LDR's purpose is to make it easy for Data Grid administrators to manage replicas, the replication process, and access mechanisms within a Data Grid.</p>
 
-<h2>High level design thoughts</h2>
+<p>Each site in an LDR-based Data Grid takes on a combination of three different roles: publisher, provider, and subscriber. A site acting as a publisher provides information about available files and where they are located (via URLs), as well as information about the files themselves (metadata such as the creation time, size, and contents). A site acting as a provider provides files that can be accessed by users and replicated at other sites. A site acting as a subscriber maintains replicas of data obtained from a provider. A Data Grid must have at least one site acting as a publisher, at least one acting as a provider, and at least one acting as a subscriber. Again, an individual site can serve as any combination of publisher, provider, or subscriber.</p>
 
-<p>
-A <i>DataGrid</i> is a set of geographically distributed sites,
-institutions, or groups with each site offering Grid Services, and
-where the set of sites has some collective need to share, distribute,
-and replicate large sets
-of data, files, or data files (if you prefer). LDR is designed to 
-replicate data among the sites in a DataGrid.
-</p>
+<p>LDR uses a metadata database to store information about what files exist and information about the files such as size or creation time. Metadata is replicated across sites so that all sites in the Data Grid are aware of all available data. Using metadata queries, Data Grid administrators define collections of files to be replicated.</p>
 
-<p>
-The LDR installation at each site takes on any combintation of three
-different roles. The roles are that of <i>publisher</i>,
-<i>provider</i>, and <i>subscriber</i>. A LDR installation acting
-as a publisher introduces into the DataGrid information 
-about available files and where they are located
-(via URLs), as well as information about the files themselves
-(so-called metadata such as the creation time or size).
-A LDR installation acting as a provider makes files available for
-replication to other sites. A LDR installation acting as
-a subscriber replicates data from a provider to itself.
-</p>
+<p>LDR assumes a very general model for "storage." The details of any particular storage system can be coded and made available to LDR via a very simple API. The "storage system" must be able to accept a file for ingestion and return a URL for the ingested file. This URL is then published in the local LRC. LDR does not need to know the details of how the file is ingested or how the URL is generated. </p>
 
-<p>A DataGrid must have at least one LDR
-installation acting as a publisher, at least one acting as a provider,
-and at least one acting as a subscriber. Again, any single LDR
-installation can be any combination of publisher, provider, and
-subscriber.
-</p>
+<p>LDR uses the Globus Replica Location Service (RLS) to store information about what files are located where. RLS is itself a distributed system: each site runs a Local Replica Catalog (LRC) to store information about the data maintained at the local site and a Replica Location Index (RLI) that aggregates information from all of the sites. Hence, each site has both a local and a global view of the data.</p>
 
-<p>
-LDR is designed to maximize the quantity of data replicated at the
-expense of the reliability of any single file being transfered. Put
-another way, LDR tries to replicate as much data as it can without
-regard to the order in which files are replicated or the success of
-any single file transfer. LDR is designed for bulk replication of
-data, as opposed to replicating smaller sets of files 
-for just-in-time computing.
+<p>LDR uses a simple priority queue for scheduling data transfers. The local RLI is used to identify a site that has a source file, and the LRC at that site is then queried to obtain the URL of the file. LDR regularly refreshes the queue based on need lists and the current state of the LRCs and RLIs.</p>
 
+<p>LDR is designed to maximize the quantity of data replicated at the expense of the reliability of any single file being transferred. Put another way, LDR tries to replicate as much data as it can without regard to the success of any single file transfer. LDR is designed for bulk replication of data, as opposed to replicating smaller sets of files for just-in-time computing.</p>
 
-<h2>Mid level design thoughts</h2>
+<p>LDR uses the GridFTP protocol to transfer data between sites. LDR attempts to transfer as many files as possible between two sites and can simultaneously transfer data between multiple sites at the same time. Failed transfers are not instantly retried. Instead, LDR moves on to the next transfer. Files that do not transfer successfully are simply rescheduled until the transfer completes successfully.</p>
 
-<p>
-Replicating data within a DataGrid requires...
+<p>The components described above are all reusable "middleware" available in open source form for use in many systems and applications. The following components, which form the coordination subsystem for LDR, are specific to LDR. They are written in Python and are implemented as server "daemons" that run at LDR sites. Different daemons will run at different sites; the daemons that run at a particular site are determined by the roles that site plays in the Data Grid (publisher, provider, subscriber).</p>
+
+<p>An LDRMaster daemon is responsible for launching other necessary LDR daemons and watching over them. The rest of the daemons are as follows.</p>
+
 <ul>
-<li>...a mechanism for keeping track of what data exists within the
-DataGrid. 
-
-<p>
-LDR uses
-a metadata database to store information about what files exist
-and information about the files such as size or creation time.
-Metadata information itself is replicated from LDR instance to LDR
-instance so that all sites in the DataGrid can be aware of what data
-might be replicated.
-
-<li>...a mechanism for keeping track of where data is. 
-
-<p>
-LDR uses
-the Globus Replica Location Server (RLS) to store information about
-what files are located where. Each RLS has two parts. The first is a
-Local Replica Catalog (LRC) to store information about what data
-exists locally. The second is a Replica Location Index (RLI) to store
-information about what LRCs exists within the DataGrid and what files
-each LRC knows about locally.
-
-<li>...a mechanism for determining what files need to replicated from one
-location to another.
-
-<p>
-A LDR administrator defines <i>collections</i> of desired files by 
-SQL queries to be performed within the LDR metadata database. 
-
-<li>...a mechanism for scheduling files to be replicated.
-
-<p>
-LDR uses a simple priority queue for scheduling.
-The source locations are
-determined by querying the local RLI to determine a LRC within the
-DataGrid that knows about a file, and then directly querying the
-remote LRC to determine the URL for the source.
-If for some reason the state of
-the queue is lost or becomes corrupt it is not a problem since LDR
-regularily regenerates the queue based on need lists and the current
-state of the LRCs and RLIs.
-
-<li>...a mechanism for actually replicating files.
-
-<p>
-LDR uses the GridFTP protocol for transfer of files across a WAN. LDR
-attempts to transfer as many files as possible between two sites and
-can simultaneously transfer data between multiple sites at the same
-time. Failed transfers are not instantly retried, but rather LDR moves
-on to the next transfer. Files that do not transfer successfully
-simply end up being rescheduled and another attempt made later.
-
-<li>...a mechanism for storing replicated files.
-
-<p>
-LDR assumes a very general model for "storage". The details of any
-particular storage system can be coded and made available to LDR via
-a very simple API. A "storage" model is only required to accept a file
-for ingestion and return a URL for the ingested file which is published
-to the local LRC. The details of how the file is ingested or the URL
-generated are not needed by LDR. 
-
+<li>LDRMetadataServer, a GSI-SOAP server that makes metadata published at a site available to other LDR instances</li>
+<li>LDRMetadataUpdate, a GSI-SOAP client that updates a LDR instance with new metadata as it is published at a remote LDR site</li>
+<li>LDRSchedule, which schedules (queues) transfers</li>
+<li>LDRTransfer, which spawns agents for each source site to replicate or transfer files from source locations</li>
+<li>LDRdataFindServer, which allows clients to query the metadata and replica location catalogs to discover data or files</li>
 </ul>
 
-<h2>Low level design thoughts</h2>
-
-<p>
-A LDRMaster daemon is responsible for launching other necessary LDR
-daemons and watching over them. The available (not all daemons are
-necessary for every LDR installation)
-daemons include
-<ul>
-<li><i>LDRMetadataServer</i>, a GSI-SOAP server that makes
-metadata published at a site available to other LDR instances.
-<li><i>LDRMetadataUpdate</i>, a GSI-SOAP client that updates
-a LDR instance with new metadata as it is published at a
-remote LDR site.
-<li><i>LDRSchedule</i>, which schedules (queues) transfers.
-<li><i>LDRTransfer</i>, which spawns agents for each source site to 
-replicate or transfer files from source locations.
-<li><i>LDRdataFindServer</i>, which allows clients to query
-the metadata and replica location catalogs to discover data or
-files
-</ul>
-
-<p>
-Each daemon is independent of any other (apart from LDRMaster), and
-each can be stopped and restarted without any need to coordinate.
-</p>
+<p>Each daemon is independent of the others, and each can be stopped and restarted without needing to coordinate with other daemons.</p>
 
 
 </div>
