2c5bb1f4f4512bba2f8c0e5f89abc50acf44a4b2
liming <liming>
2005-06-14 22:47:16 +0000

    Copied from grids-center.org and added final set of articles

---
+++ alliance/publications/clusterworld/index.php
@@ -0,0 +1,144 @@
+<?php
+
+$title = "Globus: Publications - ClusterWorld \"On the Grid\" Columns";
+
+$section = "section-2";
+include_once( "../../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+<!--
+<div id="left">
+<?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+</div>
+-->
+
+<div id="home">
+
+<h1 class="first">ClusterWorld "On the Grid" Columns</h1>
+
+<p>From December 2003 through the end of the magazine's life in May 2005, members
+   of the Globus Alliance submitted articles for 
+   <a href="http://www.clusterworld.com/">ClusterWorld</a> magazine's column,
+   <cite>On the Grid</cite>. These articles, sponsored by the NSF Middleware
+   Initiative (NMI), covered Grid issues of interest to owners and users of
+   cluster computing systems.</p>
+
+<table>
+      <tr>
+        <th>Publication Date</td>
+        <th>Title</td>
+        <th>Author(s)</td>
+      </tr>
+      <tr>
+        <td>December 2003</td>
+        <td><a href="GridFinal.pdf">Globus: An Infrastructure for Resource Sharing</a></td>
+        <td>Tom Garritano</td>
+      </tr>
+      <tr>
+        <td>January 2004</td>
+        <td><a href="0104Head.pdf">The Grid</a></td>
+        <td>Ian Foster</td>
+      </tr>
+      <tr>
+        <td>January 2004</td>
+        <td><a href="0104Grid.pdf">Security and Credential Management on the Grid</a></td>
+        <td>Sam Lang and Sam Meder</td>
+      </tr>
+      <tr>
+        <td>February 2004</td>
+        <td><a href="0204Grid.pdf">So You Want to Set Up a Grid</a></td>
+        <td>Jennifer M. Schopf and Keith R. Jackson</td>
+      </tr>
+      <tr>
+        <td>March 2004</td>
+        <td><a href="0304Grid2.pdf">Using the Globus Toolkit with Firewalls</a></td>
+        <td>Olle Mulmo and Von Welch</td>
+      </tr>
+      <tr>
+        <td>April 2004</td>
+        <td><a href="0504GridFinal.pdf">Standardizing the Grid</a></td>
+        <td>Lee Liming, Tom Garritano, Steve Tuecke</td>
+      </tr>
+      <tr>
+        <td>May 2004</td>
+        <td><a href="0404GridFinal.pdf">The Globus&#153; eXtensible Input/Output (XIO) System</a></td>
+        <td>William Allcock and John Bresnahan</td>
+      </tr>
+      <tr>
+        <td>June 2004</td>
+        <td><a href="0604GridFinal.pdf">Testing in the Grid Environment</a></td>
+        <td>Charles A. Bacon</td>
+      </tr>
+      <tr>
+        <td>July 2004</td>
+        <td><a href="0704GridFinal.pdf">Grid Packaging Software</a></td>
+        <td>Scott Gose</td>
+      </tr>
+     <tr>
+        <td>August 2004</td>
+        <td><a href="0804GridFinal.pdf">Monitoring Clusters and Grids</a></td>
+        <td>Jennifer M. Schopf and Ben Clifford</td>
+      </tr>
+       <tr>
+        <td>September 2004</td>
+        <td><a href="0904GridFinal.pdf">Maximizing Your Globus Toolkit&#153; GridFTP Server</a></td>
+        <td>William Allcock and John Bresnahan</font></td>
+      </tr>
+      <tr>
+        <td>October 2004</td>
+        <td><a href="1004GridFinal2.pdf">Programming with the GridFTP Client Library</a></td>
+        <td>William Allcock</td>
+      </tr>
+      <tr>
+        <td>November 2004</td>
+        <td><a href="1104Grid.pdf">MPICH-G2: An MPI for Grids</a></td>
+        <td>Nicholas T. Karonis</td>
+      </tr>
+      <tr>
+        <td>December 2004</td>
+        <td><a href="1204GridFinal.pdf">Is the Grid Delivering for Business?</a></td>
+        <td>Mark Parsons and Paul Graham</td>
+      </tr>
+      <tr>
+        <td>January 2005</td>
+        <td><a href="0105Grid-final.pdf">Lessons Learned from Grid Deployment</a></td>
+        <td>Lee Liming</td>
+      </tr>
+      <tr>
+        <td>February 2005</td>
+        <td><a href="0205Grid-final.pdf">MDS: The Gobus Monitoring and Discovery System</a></td>
+        <td>Ben Clifford</td>
+      </tr>
+      <tr>
+        <td>March 2005</td>
+        <td><a href="0305Grid.pdf">Web Services and the Grid</a></td>
+        <td>Lee Liming</td>
+      </tr>
+      <tr>
+        <td>April 2005</td>
+        <td><a href="0405Grid.pdf">The Globus<sup>&reg;</sup> Toolkit Replica Location Service</a></td>
+        <td>Ann L. Chervenak</td>
+      </tr>
+      <tr>
+        <td colspan=3><br>The following articles were submitted but did not appear in ClusterWorld.<br><br></td>
+      </tr>
+      <tr>
+        <td>May 2005</td>
+        <td><a href="0505Grid.pdf">GT4: What's In It For You?</a></td>
+        <td>Lee Liming and Ian Foster</td>
+      </tr>
+      <tr>
+        <td>June 2005</td>
+        <td><a href="0605Grid.pdf">Prototyping Simple Access to Visualization Resources</a></td>
+        <td>Joseph A. Insley and Michael E. Papka</td>
+      </tr>
+      <tr>
+        <td>July 2005</td>
+        <td><a href="0705Grid.pdf">Large-scale Data Replication for LIGO</a></td>
+        <td>Lee Liming</td>
+      </tr>
+    </table>
+
+</div>
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
