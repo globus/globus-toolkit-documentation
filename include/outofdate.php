cb013ee4633cc996c09ae21be3d2535ea8a994f4
cristina <cristina>
2005-06-15 17:25:47 +0000

    third attempt

--- include/outofdate.php
+++ include/outofdate.php
@@ -8,14 +8,16 @@ function globus_out_of_date()
 
 $uri = $_SERVER{'REQUEST_URI'};
 
-# check the docs for outdated and development parts if ( preg_match("/^\/docs\//", $uri) ) {
+# check the docs for outdated and development parts 
+
+if ( preg_match("/^\/docs\//", $uri) ) {
 
     if ( preg_match("/^\/docs\/development\//", $uri) ) {
         print "<div id="obsolete"  align="center"><p>This document is a work-in-progress and applies to this development release.</p></div>";
     }
     else if ( ! (preg_match("/^\/docs\/4.0\//", $uri) or
                  preg_match("/^\/docs\/3.2\//", $uri) ) ) {
-        print "<div id="obsolete"  align="center"><p>This information is for a release that is no longer supported by the Globus Project. The currently supported versions of the Globus Toolkit are 4.0 (recommended) and 3.2.</p></div>";
+        print "<div id="obsolete"  align="center"><p>This information is for a release that is no longer supported by the Globus Toolkit. The currently supported versions of the Globus Toolkit are 4.0 (recommended) and 3.2.</p></div>";
     }
 }
 }
