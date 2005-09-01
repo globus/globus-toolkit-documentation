3b326ccb5077f1aa7f8ab09f75b0373e4a8d822a
liming <liming>
2005-09-01 04:02:09 +0000

    First version

---
+++ alliance/logos/index.php
@@ -0,0 +1,82 @@
+<?php
+
+$title = "Globus: Alliance - Using Our Logos";
+$section = "section-2";
+include_once( "../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+
+<!--
+<div id ="left">
+<?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+</div>
+-->
+
+<div id="main">
+
+<!-- content STARTS here -->
+
+<h1 class="first">Using Our Logos</h1>
+
+<p>This page contains renderings of the Globus Alliance and Globus Toolkit logos
+for use in printed materials of all kinds. Please observe trademark restrictions
+and do not misuse these logos. Direct inquiries and requests to 
+<a href="mailto:info@globus.org">info@globus.org</a>.</p>
+
+<h2>Which Logo Should You Use?</h2>
+
+<p>Globus Alliance or Globus Toolkit? In simplest terms, it depends on whether you
+are referring to software or to people. The 
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]; ?>">Globus Toolkit</a> is an open source,
+community-driven software product with many authors and contributors. The 
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]; ?>">Globus Alliance</a> is a specific 
+group of people who work together on Grid problems and 
+solutions and who make frequent contributions to the Globus Toolkit.</p>
+
+<table>
+<tr>
+
+<td><img src="alliance-small.gif" width="252" height="72" alt="Globus Alliance">
+
+<p>The Globus Alliance logo is restricted for use by 
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."team/"; ?>">Globus Alliance members</a>
+and 
+<a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."affiliates.php"; ?>">affiliates</a> 
+with permission. This logo is protected by trademark and we
+are obligated to protect this trademark by preventing unauthorized use. If you
+are not formally a Globus Alliance member and you wish to use this logo,
+please send email to <a href="mailto:info@globus.org">info@globus.org</a>
+with a description of the use you have in mind.</p>
+
+<p><a href="alliance-large.eps">EPS 2048x510</a> (5.1Mb)<br>
+<a href="alliance-large.pct">PICT 2048x510</a> (136kb)<br>
+<a href="alliance-large.tif">TIFF 2048x510</a> (168kb)<br>
+<a href="alliance-small.gif">GIF 252x72</a> (4kb)</p></td>
+
+<td><img src="toolkit-small.gif" width="250" height="72" alt="Globus Toolkit">
+
+<p>The Globus Toolkit logo is for use by 
+<a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."contributors.html/"; ?>">contributors</a> 
+to the Globus Toolkit.
+Contributors are encouraged to use this logo in documentation and other 
+materials relating to their contributions. This logo is protected by trademark.
+If you are not a Globus Toolkit contributor and you wish to use this logo,
+please send email to <a href="mailto:info@globus.org">info@globus.org</a> 
+with a description of the use you have in mind.</p>
+
+<p><a href="toolkit-large.eps">EPS 2048x507</a> (5.2Mb)<br>
+<a href="toolkit-large.pct">PICT 2048x507</a> (132kb)<br>
+<a href="toolkit-large.tif">TIFF 2048x507</a> (164kb)<br>
+<a href="toolkit-small.gif">GIF 250x72</a> (4kb)</p></td>
+
+</tr>
+</table>
+
+</div>
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
+
+
+
+
+
+
