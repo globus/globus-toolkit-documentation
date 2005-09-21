7853cd527c586607880686b09c3d57007871e458
cristina <cristina>
2005-09-21 17:20:20 +0000

    updating alumni link

--- alliance/team/alumni.php
+++ alliance/team/alumni.php
@@ -1,68 +1,136 @@
 <?php
 
+
+
 $title = "Globus: Alliance - Paricipants";
+
 $section = "section-2";
+
 include_once( "../../include/local.inc" );
+
 include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+
 ?>
+
 <!--
+
 <div id="left">
+
 <?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+
 </div>
+
 -->
 
+
+
 <div id="main">
+
 <!-- content STARTS here -->
+
+
 
 <h1 class="first">Alliance Alumni</h1>
 
+
+
 <p>The following former members of the core Globus Alliance team made significant
+
       contributions to our work.</p>
 
+
+
 <h2><a href="http://www.mcs.anl.gov">Argonne National Laboratory</a>, 
+
     <a href="http://www.cs.uchicago.edu/">The University of Chicago</a></h2>
 
+
+
       <ul>
+
 	<li><a href="http://people.cs.uchicago.edu/~dangulo/">Dave Angulo</a></li>
+
 	<li>Tom Garritano</li>
+
       <li>Sam Lang</li>
+
       <li>Joe Link</li>
+
       <li>Greg Nawrocki (now President of the Globus Consortium)</li>
+
       <li><a href="http://www.gridwisetech.com/team">Pawel Plaszczak</a></li>
+
       <li>Darcy Quesnel</li>
+
       <li>Alain Roy</li>
+
 	<li>Michael Russell</li>
+
       <li><a href="http://www.mcs.anl.gov/~sander">Volker Sander</a></li>
+
       <li>Rob Seed</li>
+
 	<li>Michelle Wang</li>
+
         <li><a href="http://www-fp.mcs.anl.gov/~wulf/">Julie Wulf-Knoerzer</a></li>
+
 	</ul>
+
+
 
 <h2><a href="http://www.isi.edu">University of Southern California Information Sciences Institute</a></h2>
 
+
+
         <ul>
+
         <li>Alain Andrieux</li>
+
 	<li>Joseph Bannister</li>
-        <li>Ben Clifford</li>
+
+        <li><a href="http://www.hawaga.org.uk/ben/">Ben Clifford</a></li>
+
 	<li>Nabil Deeb</li>
+
 	<li>Steve Fitzgerald</li>
+
 	<li>Leon Kuntz (in memoriam)</li>
+
 	<li>Bob Lindell</li>
+
 	<li>Clifford Neuman</li>
+
 	<li>Tatyana Ryutov</li>
+
     </ul>
+
    
+
 <h2><a href="http://www.ncsa.uiuc.edu/SCD/Alliance/VMR/Admin/Globus/NCSA.Globus.html">National
+
     Center for Supercomputing Applications at the University of Illinois Urbana-Champaign</a></h2>
 
+
+
       <ul>
+
       <li>Eugene Dougherty</li>
+
       <li>Virginia Hudak-Davis</li>
+
       <li>Jason Novotny</li>
+
       <li>Andrea Van Proyen</li>
+
     </ul>
+
      
+
+
 
 </div>
 
+
+
 <?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
+
