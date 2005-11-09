b14f430c718f50c61caf5c1f9e287041fd483ab6
liming <liming>
2005-11-09 21:47:49 +0000

    First cut

--- alliance/events/sc05/index.php
+++ alliance/events/sc05/index.php
@@ -7,8 +7,45 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
 
 <div id="main">
 
+<a href="http://sc05.supercomputing.org/"><img align="right" border=0 height=180 width=127 src="sc05_logo.gif" alt="SC|05" \></a>
+
 <h1>The Globus Alliance at SC|05</h1>
 
-</div>
-<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
+<h2>Events</h2>
 
+<p>The Globus Alliance is providing the following elements of the SC|05 program.</p>
+
+<ul>
+<li><a href="Booth.pdf">Exhibits and presentations at the Argonne National Laboratory Booth (R1629)</a></li>
+<li><a href="SC05-GT4-BOF.pdf">Birds of a Feather - The Globus Toolkit v4: Status and Experiences</a></li>
+<li><a href="SC05-Perf-BOF.pdf">Birds of a Feather - Grid Applications and Performance</a></li>
+<li><a href="http://sc05.supercomputing.org/schedule/event_detail.php?evid=5046">Paper - The Globus Striped GridFTP Framework and Server</a></li>
+<li><a href="http://sc05.supercomputing.org/schedule/event_detail.php?evid=5098">Paper - DI-GRUBER: A Distributed Approach for Grid Resource Brokering</a></li>
+<li><a href="http://sc05.supercomputing.org/schedule/event_detail.php?evid=5102">Tutorial S14 - The Globus Toolkit in e-Science: Examples and Lessons</a></li>
+</ul>
+
+<h2>Information</h2>
+
+<p>The Globus Alliance is distributing information to SC|05 attendees from our
+base in the Argonne National Laboratory booth. The following materials are included
+in our information packets this year.</p>
+
+<p><b><a href="GT4.pdf">Globus Toolkit Version 4: Software for Service-Oriented Systems.</a></b>
+   I. Foster.  <em>IFIP International Conference on Network and Parallel Computing</em>, 
+   Springer-Verlag LNCS 3779, pp 2-13, 2005.</p>
+
+<p><b>Service-Oriented Science</b>. I. Foster. <em>Science</em>, vol. 308, May 6, 2005.</p>
+
+<p><b><a href="MDS4-sc05-v3b.pdf">MDS4: The GT4 Monitoring and Discovery System</a></b></p>
+
+<p><b><a href="gridshib-sc05.pdf">GridShib: Integrating identity federation (Shibboleth) 
+   with the Globus Toolkit to provide attribute-based authorization for distributed 
+   scientific communities.</a></b></p>
+
+<p><b><a href="DASH_SC05.pdf">DASH: Database for Secure Hyperinfrastructure</a></b></p>
+
+<p><b><a href="ESF_SC05_Flyer_OSG.pdf">ESF: Edge Services Framework for Computational Grids</a></b></p>
+
+</div>
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
