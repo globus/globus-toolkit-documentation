659bd4a8bcf07faf2638d06c3ecb4ea06b38c7e7
liming <liming>
2005-11-09 19:19:37 +0000

    Placeholder

---
+++ alliance/events/sc05/index.php
@@ -0,0 +1,14 @@
+<?php
+$title = "Globus: The Globus Alliance at SC|05";
+$section = "section-2";
+include_once( "../../../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+
+<div id="main">
+
+<h1>The Globus Alliance at SC|05</h1>
+
+</div>
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
+
