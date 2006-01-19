6b65c01d6ea470a277e849e3952408c96fe1fe57
cristina <cristina>
2006-01-19 00:34:26 +0000

    updating message for dev docs

--- include/outofdate.php
+++ include/outofdate.php
@@ -15,7 +15,9 @@ if ( preg_match("/^\/toolkit\/docs\/.+\//", $uri) ) {
     if ( preg_match("/^\/toolkit\/docs\/development\//", $uri) ) {
 		# only show one subdirs to development
 	    if ( preg_match("/^\/toolkit\/docs\/development\/.+\//", $uri) ) {
-        	print "<div id='obsolete'  align='center'><p>This document is a work-in-progress and applies to this development release.</p></div>";
+        	print "<div id='obsolete'  align='center'><p>This document is a work-in-progress and applies to this development release.  The latest drafts of docs can be found in the <a href='http://www.globus.org/toolkit/docs/development/'>Development Documentation directory</a>. <br><br>
+	You are strongly encouraged to file bugs for both the development documentation and software on our <a href='http://www-unix.globus.org/toolkit/bugzilla.html'>Bugzilla page</a>. <br><br>
+We appreciate your participation.</p></div>";
 		}
     }
     else if ( ! (preg_match("/^\/toolkit\/docs\/4.0\//", $uri) or
