c42f8f220cf69fd24f2a93081923abe28496b881
cristina <cristina>
2006-08-23 21:46:26 +0000

    adding initial info for this year's conference handouts

---
+++ alliance/events/sc06/index.php
@@ -0,0 +1,108 @@
+<?php
+
+$title = "Globus: The Globus Alliance at SC|06";
+
+$section = "section-2";
+
+include_once( "../../../include/local.inc" );
+
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+
+?>
+
+
+
+
+<div id="main">
+
+
+
+<a href="http://sc06.supercomputing.org/"><img align="right" border=0 height=78 width=150 src="logo_sc06.gif" alt="SC|06" \></a>
+
+
+
+<h1>The Globus Alliance at SC|06</h1>
+
+
+
+<p>Globus Alliance members won't be hard to find at SC06 in Tampa, November
+  11-17, 2006.
+
+Our activities include a half-day tutorial on Sunday, two "birds of a feather" meetings
+
+on Tuesday, paper presentations, and continuous exhibits and presentations at
+the Argonne 
+
+National Laboratory booth (R1629) in the exhibiter hall.</p>
+
+
+
+<h2>Events</h2>
+
+
+
+<p>Alliance members are providing the following components of the SC|06 program.</p>
+
+
+
+<ul>
+
+<li>Exhibits and presentations at the Argonne National Laboratory Booth (R1629)</li>
+
+<li>Birds of a Feather - The Globus Toolkit v4: Status and Experiences</li>
+
+<li>Birds of a Feather - Grid Applications and Performance</li>
+
+<li>Paper - The Globus Striped GridFTP Framework and Server</li>
+
+<li>Paper - DI-GRUBER: A Distributed Approach for Grid Resource Brokering</li>
+
+<li>Tutorial S14 - The Globus Toolkit in e-Science: Examples and Lessons</li>
+
+</ul>
+
+
+
+<h2>Information</h2>
+
+
+
+<p>We are distributing information to SC|06 attendees from our
+
+base in the Argonne National Laboratory booth. The following materials are included
+
+in our information packets this year.</p>
+
+
+
+<p><a href="globus_jcst_2006.pdf"><strong>Globus Toolkit Version 4: Software for Service-Oriented Systems.</strong></a>
+
+   I. Foster.  <em>IFIP International Conference on Network and Parallel Computing</em>, 
+
+   Springer-Verlag LNCS 3779, pp 2-13, 2005.</p>
+
+
+
+<p><strong><a href="MDS4-gw06-v1.pdf">MDS4: The GT4 Monitoring and Discovery System</a></strong></p>
+
+
+
+<p><strong><a href="RLSgw2006handout.pdf">The Globus Replica Location Service</a></strong></p>
+
+
+
+<p><strong><a href="DRSgw2006handout.pdf">The Globus Data Replication Service</a></strong></p>
+
+<p><strong><a href="GlobusWorld2006_ServMark.pdf">ServMark: A Distributed Grid
+      and Services Testing Framework</a></strong></p>
+
+<p><strong><a href="SCEC_Grid_factsheet_v5.pdf">Advancing SCEC Earthquake System
+          Science Research with Gridbased High Performance Computing and Scientific
+      Workflows</a></strong></p>
+
+</div>
+
+
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
+
