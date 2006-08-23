9c6dc9ad565c07696d1fa31be2c8a34551927cbd
cristina <cristina>
2006-08-23 23:16:41 +0000

    adding a blurb at the top letting people know this is in development

--- alliance/events/sc06/index.php
+++ alliance/events/sc06/index.php
@@ -15,7 +15,7 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
 
 <div id="main">
 
-<div id="solutions-sidebar" style="width: 100%; padding: 10px; ">
+<div id="solutions-sidebar" style="width: 100%; padding: 5px; margin: 5px;">
 <p>Notice: We are currently developing this page for this year's SC. This information is in flux but please check back later for more complete information. </p>
 </div>
 
