6a003e4c4032b231823b21b605a631949e76e952
cristina <cristina>
2006-08-28 22:55:20 +0000

    adding files for new globusworld 2006 page

---
+++ alliance/events/gw06/index.php
@@ -0,0 +1,112 @@
+<?php
+
+$title = "Globus: The Globus Alliance at SC|06";
+
+$section = "section-2";
+
+include_once( "../../../include/local.inc" );
+
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+
+?>
+
+
+
+
+<div id="main">
+
+<div id="solutions-sidebar" style="	width: 75%; color: #333333; font-size: 10px; line-height: 130%;	text-align: center;	margin: 10px auto; padding: 5px;">
+<p>Notice: We are currently developing this page for this year's SC. This information is in flux but please check back later for more complete information. </p>
+</div>
+
+<a href="http://sc06.supercomputing.org/"><img align="right" border=0 height=162 width=552 src="Grid_enterbanner.gif" alt="SC|06" \></a>
+
+
+
+<h1>The Globus Alliance at Globus<span style="white-space: nowrap;"><font class="header2005">WORLD</font></span> 2006</h1>
+
+
+
+<p><span style="white-space: nowrap;"><font class="header2005"> GlobusWORLD 2006
+      joins forces with</font><font class="headerTagLine"> IDG World Expo and
+      GGF</font><font class="header2005"> at GridWorld 2006 in Washington DC,
+      September 11-17th 2006. </font></span> Our activities include [update for
+      this conference: a half-day tutorial on Sunday, two "birds of a feather" meetings
+  
+on Tuesday, paper presentations, and continuous exhibits and presentations at
+  the Argonne 
+  
+National Laboratory booth (R1629) in the exhibiter hall].</p>
+<h2>Events</h2>
+
+
+
+<p>Alliance members are providing the following components of the <span style="white-space: nowrap;"><font class="header2005">GlobusWORLD
+  2006</font></span>  program. [update for this conference]:</p>
+
+
+
+<ul>
+
+<li>Exhibits and presentations at the Argonne National Laboratory Booth (R1629)</li>
+
+<li>Birds of a Feather - The Globus Toolkit v4: Status and Experiences</li>
+
+<li>Birds of a Feather - Grid Applications and Performance</li>
+
+<li>Paper - The Globus Striped GridFTP Framework and Server</li>
+
+<li>Paper - DI-GRUBER: A Distributed Approach for Grid Resource Brokering</li>
+
+<li>Tutorial S14 - The Globus Toolkit in e-Science: Examples and Lessons</li>
+
+</ul>
+
+
+
+<h2>Information</h2>
+
+
+
+<p>We are distributing information to <span style="white-space: nowrap;"><font class="header2005">GlobusWORLD
+      2006</font></span> attendees from our
+
+base in the Argonne National Laboratory booth. The following materials are included
+
+in our information packets this year.</p>
+
+
+
+<p><a href="../sc06/globus_jcst_2006.pdf"><strong>Globus Toolkit Version 4: Software for Service-Oriented Systems.</strong></a>
+
+   I. Foster.  <em>IFIP International Conference on Network and Parallel Computing</em>, 
+
+   Springer-Verlag LNCS 3779, pp 2-13, 2005.</p>
+
+
+
+<p><strong><a href="../sc06/RLSgw2006handout.pdf">The Globus Replica Location Service</a></strong></p>
+
+
+
+<p><strong><a href="../sc06/DRSgw2006handout.pdf">The Globus Data Replication Service</a></strong></p>
+
+<p><strong><a href="../sc06/GlobusWorld2006_ServMark.pdf">ServMark: A Distributed Grid
+      and Services Testing Framework</a></strong></p>
+
+<p><strong><a href="../sc06/SCEC_Grid_factsheet_v5.pdf">Advancing SCEC Earthquake System
+          Science Research with Gridbased High Performance Computing and Scientific
+      Workflows</a></strong></p>
+	  
+	  <p><strong><a href="../sc06/JWSCoreHandout.pdf">GT4 Java WS Core</a></strong></p>
+	  <p><strong><a href="../sc06/Flyer_for_GridWay_ProtoProject.pdf">Gridway - Metascheduling Technologies for the Grid</a> </strong></p>
+	  
+	   <p><strong><a href="../sc06/GWHandout-RFT.pdf">Reliable File Transfer</a> </strong></p>
+	   
+	   <p><strong><a href="../sc06/gt-authz-gw06-v3.pdf">The Globus Toolkit Authorization Framework</a> </strong></p>
+</div>
+
+
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
+
