f731a538a7d42b705a9c326a12bc09cfc31df256
liming <liming>
2006-09-26 14:44:52 +0000

    Added Google Analytics code

--- index.php
+++ index.php
@@ -111,6 +111,12 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
 </div>
 
 
+<script src="http://www.google-analytics.com/urchin.js" type="text/javascript">
+</script>
+<script type="text/javascript">
+_uacct = "UA-726125-2";
+urchinTracker();
+</script> 
 
 </body>
 </html>
