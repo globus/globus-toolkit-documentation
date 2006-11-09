386152d9c5e9e0e6ed45868010d907563ff82ece
cristina <cristina>
2006-11-09 00:22:16 +0000

    adding new presentation files per jen

--- alliance/events/sc06/index.php
+++ alliance/events/sc06/index.php
@@ -13,6 +13,7 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
 
 
 
+
 <div id="main">
 
 <div id="solutions-sidebar" style="	width: 75%; color: #333333; font-size: 10px; line-height: 130%;	text-align: center;	margin: 10px auto; padding: 5px;">
@@ -60,50 +61,49 @@ base in the Argonne National Laboratory booth. The following materials are inclu
 
 in our information packets this year.</p>
 
-
-
 <div id="main">
-  <p><a href="globus_jcst_2006.pdf"><strong>Globus Toolkit Version 4: Software
-        for Service-Oriented Systems.</strong></a> I. Foster. <em>IFIP International
-        Conference on Network and Parallel Computing</em>, Springer-Verlag LNCS
-        3779, pp 2-13, 2005.</p>
-  <p><strong><a href="foster-DataGridTools.pdf">Data Grid Tools: Enabling Science
-        on Big Distributed Data</a></strong></p>
-  <p><strong><a href="DATAsc2006handoutFinal.pdf">Globus&reg; Data Services</a> </strong></p>
-  <p><strong><a href="VDS_brochure_v4.pdf">The VDS Workflow Solution Kit</a></strong></p>
-  <p><strong><a href="gt-authz-sc06-v3.pdf">The Globus&reg; Toolkit Authorization
-        Framework</a> </strong></p>
-  <p><strong><a href="CDIGS-SC-handout-final.pdf">Community Driven Improvement
-        of Globus Software (CDIGS)</a></strong></p>
-  <p><strong><a href="roadProcessOverviewPic2.pdf">Globus&reg; Roadmap</a></strong></p>
-  <p><strong><a href="devglobus_and_incubator_SC06_v4.pdf">dev.globus.org and
-        the Globus&reg; Incubator Process</a> </strong></p>
-  <p><strong><a href="MDS4-sc06-v4.pdf">MDS4: The GT4 Monitoring and Discovery
-        System</a> </strong></p>
-  <p><strong><a href="GRAM-sc06-flyer-v5.pdf">GRAM: GT's Job Submission Service</a></strong></p>
-  <p><strong><a href="JWSCoreHandout.pdf">GT4 Java WS Core</a></strong></p>
-  <p><strong><a href="SC_C_WS_Core_Handout.pdf">Globus&reg; Toolkit C Web Services
-        Core</a> </strong></p>
-  <p><strong><a href="RLSsc2006handout.pdf">The Globus Replica Location Service</a></strong></p>
-  <p><strong><a href="DRSsc2006handout.pdf">The Globus Data Replication Service</a></strong></p>
-  <p><strong><a href="GridFTP_Overview.pdf">The Globus&reg; Striped GridFTP Framework
-        and Server</a></strong></p>
-  <p><strong><a href="SC_Security_Handout.pdf">Globus&reg; Toolkit Java Security
-        Components</a> </strong></p>
-  <p><strong><a href="SCHandout-RFT.pdf">Reliable File Transfer</a> </strong></p>
-  <p><strong><a href="UGEDataSheet.pdf">Univa Globus&reg; Enterprise</a></strong></p>
-  <p><strong><a href="SCEC_Grid_factsheet_v5.pdf">Advancing SCEC Earthquake System
-        Science Research with Gridbased High Performance Computing and Scientific
-        Workflows</a></strong></p>
-  <p><strong><a href="SC2006_ServMark.pdf">ServMark: A Distributed Grid
-        and Services Testing Framework</a></strong></p>
-  <p><strong><a href="Flyer_for_GridWay_ProtoProject.pdf">Gridway - Metascheduling
-        Technologies for the Grid</a> </strong></p>
+  <p><a href="#"><strong>Schedule</strong></a></p>
+		
+        <p><strong>Overview</strong></p>
+        <ul>
+          <li> <a href="globus_jcst_2006.pdf"><strong>Globus Toolkit Version
+                  4: Software for Service-Oriented Systems.</strong></a> I. Foster. <em>IFIP
+                  International Conference on Network and Parallel Computing</em>,
+                  Springer-Verlag LNCS 3779, pp 2-13, 2005.</li>
+          <li> <a href="dev_globus-incubator.pdf"><strong>dev.globus.org
+          and the Globus&reg; Incubator Process</strong></a> </li>
+          <li> <strong>Globus&reg; Roadmap (still
+    waiting on this)</strong></li>
+          <li> <strong><a href="CDIGS.pdf">Community Driven
+          Improvement of Globus Software (CDIGS)</a></strong></li>
+          <li><strong><a href="cedps.pdf">CEDS</a></strong></li>
+        </ul>
+        <p><strong>Security and Core Components</strong></p>
+        <ul>
+          <li><strong><a href="Security.pdf">Security</a></strong></li>
+          <li> <strong><a href="AuthZ.pdf">Authorization Framework </a></strong></li>
+          <li> <strong><a href="JWSCore.pdf">Java WS Core</a></strong></li>
+          <li> <strong><a href="C_WS_Core.pdf">C WS Core</a> </strong></li>
+        </ul>
+<p><strong>Data Management</strong></p>
+<ul>
+  <li><strong>Globus&reg; Data Services </strong></li>
+  <li> <strong><a href="GridFTP.pdf">GridFTP</a></strong></li>
+  <li> <strong><a href="rft.pdf">Reliable File Transfer (RFT) </a> </strong></li>
+  <li> <strong>Replica Location Service (RLS) </strong></li>
+  <li> <strong>Data Replication Service (DRS) </strong></li>
+  <li> <strong><a href="foster-DataGridTools.pdf">Data Grid Tools: Enabling Science
+      on Big Distributed Data</a></strong></li>
+</ul>
+<p><strong>Execution management and Monitoring</strong></p>
+<ul>
+  <li> <strong>GRAM: GT's Job Submission Service</strong> (still
+    waiting on this)</li>
+  <li> <strong><a href="MDS4.pdf">MDS4: The GT4 Monitoring and Discovery
+      System </a></strong></li>
+  <li> <strong><a href="VDS.pdf">The VDS Workflow Solution Kit</a></strong></li>
+</ul>
 </div>
-<p>&nbsp;</p>
-
-
-
 </div>
 
 
