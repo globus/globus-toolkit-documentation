db5d1ed61070d608ddf567f127456deffd3aa7a5
cristina <cristina>
2006-11-13 19:17:38 +0000

    massive updates

--- alliance/events/sc06/index.php
+++ alliance/events/sc06/index.php
@@ -17,6 +17,14 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
 
 
 
+
+
+
+
+
+
+
+
 <div id="main">
 <!-- 
 <div id="solutions-sidebar" style="	width: 75%; color: #333333; font-size: 10px; line-height: 130%;	text-align: center;	margin: 10px auto; padding: 5px;">
@@ -29,9 +37,11 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
       <p>Learn about Globus and grid computing, plus meet with Globus Alliance
         members, at <a href="http://sc06.supercomputing.org/"><strong>SC|06</strong></a> in
         Tampa, November 13-17, 2006. </p>
-      <p>Our activities include two workshops and continuous exhibits and presentations
-        at the <strong>Argonne National Laboratory booth (R1925)</strong> in
+      <p>Our activities include various grid computing events   (see
+        below) as well as continuous exhibits and presentations at the <strong>Argonne
+        National Laboratory booth (R1925)</strong> in
         the Exhibit Hall of the Tampa Convention Center. </p>
+      <p><strong>We also offer most of our handouts in PDF format (see <a href="#handouts">Handouts</a>). </strong></p>
       <p>We look forward to meeting you there!</p></td>
     <td width="19%" valign="top"><a href="http://sc06.supercomputing.org/"><br/>
 	<img align="center" border="0" height="88" width="160" src="logo_sc06.gif" alt="SC|06" \></a></td>
@@ -39,38 +49,152 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
   </tr>
 </table>
 
-<h2 style="margin-top: 0; padding-top: 0; ">Workshops</h2>
-<p> <strong>November 12-13:</strong> <a href="http://www.cogkit.org/GCE06/"><strong>Second
-      International Grid Computing Environments and Portals Workshop 06</strong></a> </p>
+<h2 style="padding-top: 0; "><a name="workshops"></a>Workshops</h2>
+<p><b class="white"><a href="http://sc06.supercomputing.org/schedule/event_detail.php?evid=5108">TeraGrid
+      Institute Workshop</a></b> </p>
+<blockquote>
+  <p>Chair: Scott Lathrop<br>
+    Location: Salon H (Marriott)<br>
+    Date: Sunday, 12-Nov-06, <br>
+    Time: 
+    8 am - 3 pm</p>
+</blockquote>
+<p><a href="http://www.cogkit.org/GCE06/"><strong>Second
+        International Grid Computing Environments and Portals Workshop 06</strong></a> </p>
 <blockquote>
   <p>Chair:	Gregor von Laszewski <br>
-    Salon G<br>
+    Location: Salon G<br>
     Day 1: Sunday, 12-Nov-06, 8 am - 5 pm<br>
     Day 2: 
     Monday, 13-Nov-06,
     8 am - 3 pm</p>
 </blockquote>
-<p> <strong>November 17:
-    <a href="http://workspace.globus.org/vtdc06">Workshop on Virtualization
-    Technologies in Distributed Computing</a></strong> </p>
+<p><strong><a href="http://sc06.supercomputing.org/schedule/event_detail.php?evid=5099">Workshop on Virtualization
+  Technologies in Distributed Computing</a></strong> </p>
 <blockquote>
-  <p>General Chair: Jose
-        Fortes <br>
-        Room 17<br>
-        Friday, 17-Nov-06, 8 am - 5 pm
+  <p>        Chair: Kate Keahey
         <br>
-  </p>
+        Location: 17<br>
+        Date: Friday, 17-Nov-06<br>
+      Time: 8 am - 5 pm    </p>
+</blockquote>
+<h2><a name="talks"></a>Talks (Booth <strong>R1925</strong>)</h2>
+<p><strong>Monday, November 13 </strong></p>
+<blockquote>
+  <p>  7:00 <em>Challenges of Managing Large Scale
+      Workflows in Distributed Environments</em>, Ewa Deelman <br>
+      7:30 <em>Earth System Grid</em>,
+    Ann Chervenak <br>
+    8:00 <em>How
+      to Contribute to Globus</em>, Jennifer Schopf <br>
+      8:30 <em>Grid Workflow Tools</em>, Mike
+      Wilde </p>
+</blockquote>
+<p><strong>Tuesday, November 14 </strong></p>
+<blockquote>
+  <p>10:30 <em>PGRADE/GEMLCA Workflows to GRAM on multiple
+      production Grids</em>, Stuart Martin<br>
+      11:00 <em>GRAM: status update, plans and
+        performance</em>, Stuart Martin <br>
+      11:30 <em>How to Contribute to Globus</em>, Jennifer
+  Schopf <br>
+  12:00 <em>Data Services in Globus</em>, Ann Chervenak <br>
+  12:30 <em>Using Globus Tools
+        for Medical Data</em>, Riddhi Shah <br>
+      2:00 <em>Grid Workflow Management with Application
+  to Water Threat Management</em>, Gregor von Laszewski <br>
+  2:30 <em>SCEC Earthworks
+      Science Gateway</em>, Philip Maechling <br>
+      3:00 <em>Earth System Grid</em>, Ann Chervenak <br>
+      3:30 <em>Virtual Workspace Appliances</em>,
+      Tim Freeman and Kate Keahey </p>
+</blockquote>
+<p><strong>Wednesday,
+  November 15 </strong></p>
+<blockquote>
+  <p>10:30 <em>Earth System Grid</em> Ann Chervenak <br>
+      11:00 <em>Data Services in
+          Globus</em>, Ann Chervenak <br>
+      11:30 <em>SCEC Earthworks Science Gateway</em>, Philip Maechling <br>
+      2:00 <em>OGSA-HPCP/BES
+      demo</em>, Peter Lane <br>
+      2:30 <em>AstroPortal</em>, Ioan Raicu <br>
+      3:00
+          <em>How to Contribute to Globus</em>, Jennifer Schopf <br>
+      3:30 <em>Grid Workflow Tools</em>,
+      Mike Wilde <br>
+      4:00 <em>A Sneak Peak at What's New in GridFTP</em>, Raj Kettimuthu <br>
+      4:30 <em>Challenges
+      of Managing Large Scale Workflows in Distributed Environments</em>, Ewa Deelman
+      <br>
+      5:00 <em>Virtual Workspace Appliances</em>, Tim Freeman and Kate Keahey </p>
+</blockquote>
+<p><strong>Thursday, November 16 </strong></p>
+<blockquote>
+  <p>10:00 <em>Grid Workflow Tools</em>, Mike Wilde <br>
+      10:30
+          <em>Virtual Workspace Appliances</em>, Tim Freeman and Kate Keahey <br>
+      11:00 <em>Troubleshooting
+        and Monitoring in GT4</em>, Jennifer Schopf <br>
+      11:30 <em>Build a Grid service
+          using Introduce</em>, Ravi Madduri</p>
 </blockquote>
 
-<h2>Information</h2>
 
+<h2><a name="demos"></a>Demos</h2>
+<p><strong>vGrADS, built on top of Globus:</strong></p>
+<blockquote>
+  <p>Tue, 14-Nov, 12:00-12:30 Demo at GCAS booth <br>
+      Tue, 14-Nov, 2:30- 3:00 Talk at USC booth <br>
+      Wed,  15-Nov, 1:00- 1:30 Demo at SDSC booth<br>
+      Thu,  16-Nov, 10:30-11:00 Demo at RENCI booth</p>
+  </blockquote>
+  
+<p><strong>OGSA-HPCP/BES demo, Peter Lane:</strong></p>
+<blockquote>
+  <p>Wed, 15-Nov, 2:00 at Argonne National Laboratory booth (R1925)</p>
+</blockquote>
+<h2><a name="tutorial"></a>Tutorial</h2>
+<p><a href="http://sc06.supercomputing.org/schedule/event_detail.php?evid=5065"><strong>Introduction
+      to Grid Computing: The First Steps</strong></a> </p>
+<blockquote>
+  <p>      Presenter(s): David
+      Gehrig, Mike Freemon, Jaime Frey<br>
+      Location: 19      <br>
+    Date: Monday, November 13th<br> 
+    Time: 8:30am -5:00 pm</p>
+</blockquote>
+<h2><a name="posters"></a>Poster Sessions</h2>
+<p>The following poster sessions happen on <strong>Tuesday, November
+  14th, 5:15p - 7:15p</strong>  in the <strong>Ballroom Corridor:</strong></p>
+<ul>
+  <li><a href="http://sc06.supercomputing.org/schedule/event_detail.php?evid=5203"><strong>Harnessing
+        Grid Resources to Enable the Dynamic Analysis of Large Astronomy Datasets</strong></a> (Ioan
+        Raicu, Ian Foster, Alex Szalay)</li>
+  <li><a href="http://sc06.supercomputing.org/schedule/event_detail.php?evid=5261"><strong>To
+      Bid or Not To Bid: A Hybrid Market-Based Resource Allocation Framework</strong></a> (Ioan
+      Raicu, Ian Foster, Alex Szalay, <span class="event_info_sm">Elizeu Santos-Neto,
+      Kate Keahey</span>)</li>
+  <li><a href="http://sc06.supercomputing.org/schedule/event_detail.php?evid=5242"><strong>Designing
+      a Collaborative Cyberinfrastructure for Event-driven Coastal Modeling</strong></a> (Philip
+      Bogden, Gabrielle Allen, Gerry Creager, Sara Graves, Rick Luettich, Lavanya
+      Ramakrishnan) </li>
+  <li><a href="http://sc06.supercomputing.org/schedule/event_detail.php?evid=5259"><strong>Utilizing
+      Grid Computing Technologies for Advanced Reservoir Studies</strong></a> (Zhou
+      Lei, Gabrielle Allen, Dayong Huang, Hartmut Kaiser, Xin Li, Chris White)</li>
+  <li><a href="http://sc06.supercomputing.org/schedule/event_detail.php?evid=5252"><strong>Human
+      Arterial Tree Simulation on TeraGrid</strong></a>  (Leopold
+      Grinberg, Suchuan Dong, James Noble, Alexander Yakhot, George Karniadakis,
+      Nicholas Karonis)</li>
+</ul>
+<h2><a name="handouts"></a>Handouts</h2>
 <p>We are distributing information to SC|06 attendees from our
 
 base in the Argonne National Laboratory booth. The following materials are included
 
 in our information packets this year.</p>
 
-  <p><a href="schedule.pdf"><strong>Schedule</strong></a></p>
+  <p><a href="schedule.pdf"><strong>Schedule of Globus-related presentations and demonstrations at SuperComputing</strong></a></p>
 		
         <p><strong>Overview</strong></p>
         <ul>
@@ -102,14 +226,21 @@ in our information packets this year.</p>
   <li> <strong><a href="foster-DataGridTools.pdf">Data Grid Tools: Enabling Science
       on Big Distributed Data</a></strong></li>
 </ul>
-<p><strong>Execution management and Monitoring</strong></p>
+<p><strong>Execution Management and Monitoring</strong></p>
 <ul>
   <li> <strong><a href="GRAM.pdf">GRAM: GT's Job Submission Service</a></strong></li>
   <li> <strong><a href="MDS4.pdf">MDS4: The GT4 Monitoring and Discovery
       System </a></strong></li>
   <li> <strong><a href="VDS.pdf">The VDS Workflow Solution Kit</a></strong></li>
 </ul>
-
+<p><strong>Samples of Commercial, Research and Incubator Projects</strong></p>
+<ul>
+  <li><a href="UGEDataSheet.pdf"><strong>Univa Globus&reg; Enterprise</strong></a></li>
+  <li> <a href="SCEC_Grid_factsheet_v5.pdf"><strong>Advancing SCEC Earthquake System Science Research with Grid based High
+      Performance Computing and Scientific Workflows</strong></a></li>
+  <li> <a href="SC2006_ServMark.pdf"><strong>ServMark: A Distributed Grid and Services Testing Framework</strong></a></li>
+  <li> <a href="Flyer_for_GridWay_ProtoProject.pdf"><strong>Gridway - Metascheduling Technologies for the Grid</strong></a></li>
+</ul>
 </div>
 
 
