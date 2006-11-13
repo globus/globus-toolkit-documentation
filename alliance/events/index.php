a8fbdbaddc11568f37652bdec75c770eec099a9c
cristina <cristina>
2006-11-13 19:29:05 +0000

    weak update

--- alliance/events/index.php
+++ alliance/events/index.php
@@ -1,199 +1,407 @@
 <?php
 
+
+
 $title = "Globus: Alliance - Meetings and Events";
 
+
+
 $section = "section-2";
+
 include_once( "../../include/local.inc" );
+
 include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+
 ?>
+
 <!--
+
 <div id="left">
+
 <?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+
 </div>
+
 -->
+
+
 
 <div id="main">
 
+
+
 <h1 class="first">Meetings and Events</h1>
 
-<p>The Globus Alliance was or will be represented at the following meetings and 
-events.  The <a href="archive.php">Meetings and Events Archive</a> contains 
-information and notes from previous events.</p>
-    
-<h2>December 2005</h2>
 
+
+<p>The Globus Alliance was or will be represented at the following meetings and 
+
+events.  The <a href="archive.php">Meetings and Events Archive</a> contains 
+
+information and notes from previous events.</p>
+
+    
+
+<h2>November 2006</h2>
+<p><b><a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."events/sc06/"; ?>">SC06</a></b><br>
+November 13-17, 2006<br>
+<u>Tampa, FL </u><br>
+Globus Alliance members will <a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."events/sc06/"; ?>">showcase
+their recent work</a> at SC06. </p>
+<h2>September 2006</h2>
+<p><b><a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."events/gw06/"; ?>">GW06</a></b><br>
+September 11-17, 2006<br>
+<u>Washington, DC </u><br>
+Globus Alliance members will <a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."events/gw06/"; ?>">showcase
+their recent work</a> at GW06.  </p>
+<h2>December 2005</h2>
 <p><b>Cyberinfrastructure Seminar Series: "GridShib: An Attribute-Based Authorization Framework"</b><br>
+
 11:00am-12:30pm PDT<br>
+
 December 6, 2005<br>
+
 <u>Access Grid, SDSC Auditorium, Webcast</u><br>
+
 The Globus Toolkit is the leading toolkit for the deployment of grids. Shibboleth is a 
+
 project from the Internet2 community that allows for cross-organizational sharing of web 
+
 resources. GridShib builds on these existing technologies to allow for interoperability 
+
 between them so that virtual organizations built on Globus Toolkit can leverage 
+
 Shibboleth.  This talk provides overviews of the Globus Toolkit, Shibboleth, and GridShib, 
+
 as well as future challenges. The talk will be technical, but will not presume an 
+
 understanding of security. The Cyberinfrastructure Seminar Series is a set of 
+
 presentations on cyberinfrastructure and related research organized by NCSA and SDSC. 
+
 These seminars are available on site at the presenting institution and remotely via the 
+
 Access Grid and webcast via <a href="http://www.cichannel.org/">CI Channel</a>. For more details 
+
 regarding the AG venue for this seminar, please refer to 
+
 <a href="http://agschedule.ncsa.uiuc.edu/meetingdetails.asp?MID=11627">the AG scheduler</a>. 
+
 All Access Grid sites are welcome to participate in this seminar. If you have any questions, 
+
 contact Jennie File, NCSA Training and Outreach Group.</p>
+
+
 
 <h2>November 2005</h2>
 
+
+
 <p><b><a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."events/sc05/"; ?>">SC05</a></b><br>
+
 November 13-17, 2005<br>
+
 <u>Seattle, WA</u><br>
+
 Globus Alliance members will 
+
 <a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."events/sc05/"; ?>">showcase their recent work</a> 
+
 at SC05.</p>
 
+
+
 <p><b><a href="http://sc05.supercomputing.org/schedule/event_detail.php?evid=5102">SC05 
+
 Tutorial: The Globus Toolkit in e-Science: Examples and Lessons</a></b><br>
+
 November 13, 2005<br>
+
 <u>Seattle, WA</u><br>
+
 This half-day tutorial provides three case studies from successful e-Science projects and 
+
 highlights the lessons learned from those experiences. Three challenging IT problems will 
+
 be presented in the context of specific science projects. The tutorial will explore the 
+
 science requirements, the technical approach taken in each solution, and the results. 
+
 Reusable Grid technologies from the Globus Toolkit such as GridFTP, MyProxy, and GRAM will 
+
 be introduced. Along the way, we will highlight the general lessons that these experiences 
+
 provided and the implications for new e-Science projects.</p>
 
+
+
 <p><b><a href="http://sc05.supercomputing.org/schedule/event_detail.php?evid=5272">SC05 Birds of a Feather (BOF): Globus Toolkit v4: Status and Experiences</a></b><br>
+
 November 15, 2005<br>
+
 <u>Seattle, WA</u><br>
+
 This BOF will provide an opportunity to learn about GT4, hear from early adopters, and provide feedback on future directions.</p>
 
+
+
 <p><b><a href="http://sc05.supercomputing.org/schedule/event_detail.php?evid=5267">SC05 Birds of a Feather (BOF): Grid Applications and Performance</a></b><br>
+
 November 15, 2005<br>
+
 <u>Seattle, WA</u><br>
+
 Following activities at the Third International Grid Performance Workshop (June 2005), 
+
 this BOF will allow further discussion between tool and application stakeholders to 
+
 forward performance work in Grids.</p>
+
+
+
 
 
 <h2>October 2005</h2>
 
+
+
 <p><b><a href="http://www.ggf.org/GGF15/ggf_events_schedule_Globus.htm">GGF-15 
+
 Community Event: Building a Java Service Using The Globus Toolkit v4</a></b><br>
+
 October 6, 2005<br>
+
 <u>Boston, MA</u><br>
+
 This half-day tutorial is designed to teach developers how to build a Java Service using the Globus Toolkit (GT4). This course is a compressed version of our standard, full-day, hands-on tutorial. Attendees are invited to follow along with the exercises on the laptops, though tech support will be very limited due to the amount of material to be covered.</p>
+
+
 
 <h2>September 2005</h2>
 
+
+
 <p><b><a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."tutorials/BAS/APAC/"; ?>">Globus Toolkit Tutorials in Australia</a></b><br>
+
 September 29-30, 2005<br>
+
 <u>Royal Pines Resort, Gold Coast</u><br>
+
 This two-day tutorial will provide an in-depth introduction to programming with the 
+
 latest version of the Globus Toolkit version 4 (GT4). Released at the end of April 
+
 2005, GT4 provides a robust set of new services and tools, and completes the first 
+
 stage of the migration to Web services that began in 2003 with GT3.</p>
+
+
 
 <h2>July 2005</h2>
 
+
+
 <p><b><a href="http://www.caip.rutgers.edu/hpdc2005/tutorial.html">Half-day GT4 Tutorial at HPDC</a></b><br>
+
 July 24, 2005<br>
+
 <u>Research Triangle Park, NC</u><br>
+
 Ian Foster will present a half-day GT4 tutorial at HPDC, Sunday July 24th, in Research Triangle Park, North Carolina.
+
+
 
 <h2>June 2005</h2>
 
+
+
 <p><b><a href="ggf14/">Globus Toolkit Version 4: Status and Experiences - A Workshop at GGF-14</a></b><br>
+
 June 27, 2005<br>
+
 <u>Chicago, IL</u><br>
+
 This workshop at GGF-14 will include an introduction to the Globus Toolkit, version
+
 4.0, by members of the Globus Toolkit development community. It will also include
+
 descriptions of early experiences with GT4 in Grid application deployment projects,
+
 presented by members of the Globus Toolkit user community.</p>
 
+
+
 <p><b><a href="http://www.grids-center.org/train/June2005.asp">2005 GRIDS 
+
 Center Community Workshop</a></b><br>
+
 June 23-24, 2005<br>
+
 <u>Chicago, IL</u><br>
+
 This workshop brings together leaders and technical staff from leading 
+
 Cyberinfrastructure development and deployment projects to encourage
+
 cross-discipline interactions involving Grid middleware.  
+
 Many groups have overlapping or very similar middleware needs, and the GRIDS 
+
 leadership wants to help identify these targets of opportunity that will 
+
 maximize benefits to the greater scientific community through coordinated 
+
 effort and reduced duplication.</p>
 
+
+
 <p><b><a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."tutorials/BAS/NCSA/"; ?>">Globus 
+
 Toolkit 4 Tutorial</a></b><br>
+
 June 16-17<br>
+
 <u>Champaign-Urbana, IL</u><br>
+
 A two-day tutorial on Globus Toolkit v. 4 will be held June 16-17 at
+
 the Beckman Institute on the University of Illinois campus. The
+
 tutorial is co-sponsored by NCSA, Argonne National Laboratory, and the
+
 GRIDS Center.  The tutorial will include an overview of GT4, GT4 security, 
+
 and how to build a Java service using GT4. A
+
 <a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."tutorials/BAS/NCSA/"; ?>">full agenda 
+
 and other details</a> are available online.  
+
 The tutorial is free; to register, email Mike Freemon at
+
 <a href="mailto:mfreemon@ncsa.uiuc.edu">mfreemon@ncsa.uiuc.edu</a>.  
+
 [<a href="http://www.beckman.uiuc.edu/inside/directions.html">Directions to Beckman Institute and hotel information</a>]</p>
 
+
+
 <p><b><a href="http://www.midnetinc.org/conferences.html">2005 Great Plains 
+
 Network-MIDnet Annual Conference</a></b><br>
+
 June 8-10, 2005<br>
+
 <u>Kansas City, MO</u><br>
+
 This conference brings together the region's leaders in network technology, research 
+
 and education to explore middleware, grid computing, research and advanced network 
+
 technology. Ian Foster will deliver one of three keynote lectures, and June 10
+
 will be a full-day <a href="http://www.midnetinc.org/conferences/globus.html">Globus 
+
 Toolkit and Grid workshop</a>.</p>
+
+
 
 <h2>May 2005</h2>
 
+
+
 <p><b>2 Day Programming Tutorial with WSRF and the Globus Toolkit Version 4</b><br>
+
 May 3-4, 2005<br>
+
 <u>San Diego, CA</u><br>
+
 These two days of tutorials provide an in-depth introduction to programming with the 
+
 latest version of the Globus Toolkit (GT4).  The course is geared toward developers 
+
 who want to learn about the newest work of the Globus Alliance and how to apply 
+
 fundamental concepts in Grid computing.  The first day teaches developers how to 
+
 build a Java Service that makes use of GT4 mechanisms for state management, security, 
+
 registry, and related topics.  The second day is less hands on and more 
+
 presentation-oriented. It provides a comprehensive introduction to GT4, covering 
+
 execution management services, data movement and management services, and security services, and also 
+
 reviewing some of the higher-level tools that build on Globus mechanisms. A brief 
+
 introduction to administering a GT4-based Grid is also provided. To register,
+
 send email to <a href="mailto:gt4tutorial@novgp.com">gt4tutorial@novgp.com</a>.
+
 <a href="<?=$SITE_PATHS["WEB_TOOLKIT"]."tutorials/BAS/SDSC/"; ?>">Prep materials</a> for 
+
 the developer tutorial are available.</p>
+
+
 
 <h2>April 2005</h2>
 
+
+
 <p><b><a href="http://www.nesc.ac.uk/esi/events/519/">UK Globus Week, 2005</a></b><br>
+
 April 4-8, 2005<br>
+
 <u>Edinburgh, UK</u><br>
+
 UK Globus Week has 5 aims: to bring together people from the UK, Europe, and 
+
 globally with a significant interest in the Grid; to present Globus developments and 
+
 research; to provide a program of tutorials, workshops, Q&A, and demonstrations from 
+
 the Globus Alliance; to develops a roadmap for continued Globus activities with
+
 significant contributions from UK and European experiences and expertise; to document 
+
 the event's proceedings as a UK eScience Technical Report. 
+
 (The <a href="<?=$SITE_PATHS["WEB_ALLIANCE"]."publications/papers/UKglobus-v4.pdf"; ?>">workshop report</a> is now available.)
+
 <a class="learnmore" href="http://www.nesc.ac.uk/esi/events/519/">Learn more...</a>
+
 </p>
+
+
 
 <h2>February 2005</h2>
 
+
+
 <p><b><a href="http://www.globusworld.org/">GlobusWORLD 2005</a></b>
+
 <br>February 7-11, 2005
+
 <br><u>Boston, Mass. </u>
+
 <br>GlobusWORLD is the premier conference on Grid computing, to be held in Boston, 
+
 February 7-11, 2005. The conference features parallel plenary sessions of Globus 
+
 Toolkit and Grid-related content for attendees from both industry and science. 
+
 <a class="learnmore" href="http://www.globusworld.org">Learn more...</a></p>
 
+
+
 <p>The <a href="archive.php">Meetings and Events Archive</a> contains 
+
 information and notes from previous events.</p>
+
+
 
 </div>
 
+
+
 <?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
+
