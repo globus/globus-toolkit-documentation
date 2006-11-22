7432227ea21bdba46c45b2f7183f2e34f59de8b1
cristina <cristina>
2006-11-22 03:56:57 +0000

    adding link to work page

--- alliance/team/index.php
+++ alliance/team/index.php
@@ -1,156 +1,312 @@
 <?php
 
+
+
 $title = "Globus: Alliance - Participants";
+
 $section = "section-2";
+
 include_once( "../../include/local.inc" );
+
 include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+
 ?>
+
 <!--
+
 <div id="left">
+
 <?php include($SITE_PATHS["SERV_INC"].'alliance.inc'); ?>
+
 </div>
+
 -->
+
+
 
 <div id="main">
 
+
+
 <h1 class="first">The Globus Team</h1>
 
+
+
 <p>The following people are currently members of the core Globus Alliance team.  
+
 (See also our <a href="alumni.php">Alliance alumni</a>.)</p>
+
+
 
 <h2><a href="http://www.mcs.anl.gov">Argonne National Laboratory</a>, <a href="http://www.uchicago.edu/">University of Chicago</a></h2>
 
+
+
     <table width="100%" class="news">
+
     <tr>
+
     <td width="50%">
+
       <ul>
+
         <li><a href="http://www-fp.mcs.anl.gov/~allcock/">Bill Allcock</a></li>
+
 	<li>Rachana Anathakrishnan</li>
+
         <li><a href="http://www-unix.mcs.anl.gov/~bacon/">Charles Bacon</a></li>
+
 	<li><a href="http://www.mcs.anl.gov/~bester/">Joe Bester</a></li>
+
 	<li>Eric Blau</li>
+
 	<li>John Bresnahan</li>
+
 	<li><a href="http://www-unix.mcs.anl.gov/~childers/">Lisa Childers</a></li>
+
 	<li><a href="http://www.mcs.anl.gov/~foster/">Ian Foster</a></li>
-        <li>Tim Freeman</li>
+
+        <li><a href="http://www-unix.mcs.anl.gov/%7Etfreeman/">Tim Freeman</a></li>
+
 	<li><a href="http://www.mcs.anl.gov/~gawor">Jarek Gawor</a></li>
+
         <li>Mihael Hategan</li>
+
 	<li><a href="http://www-unix.mcs.anl.gov/~keahey/">Kate Keahey</a></li>
+
 	<li><a href="http://www-unix.mcs.anl.gov/~kettimut/">Raj Kettimuthu</a></li>
+
         <li>Deepti Kodeboyina</li>
+
       </ul>
+
     </td>
+
     <td width="50%">
+
       <ul>
+
 	<li><a href="http://www-unix.mcs.anl.gov/~lane/">Peter Lane</a></li></li>
+
 	<li><a href="http://www.mcs.anl.gov/~gregor/">Gregor von Laszewski</a></li>
+
 	<li><a href="http://www-unix.mcs.anl.gov/~liming/">Lee Liming</a></li>
+
 	<li><span lang="en-us">Mike Link</span></li>
+
 	<li><a href="http://www-unix.mcs.anl.gov/~madduri/">Ravi Madduri</a></li>     
+
 	<li>Stuart Martin</li>
+
         <li>Neill Miller</li>
+
 	<li>Veronika Nefedova</li>
+
 	<li><a href="http://www-fp.mcs.anl.gov/~rohder/">Gigi Rohder</a></li>
+
         <li>Doug Scheftner</li>
+
 	<li><a href="http://www-unix.mcs.anl.gov/~schopf/">Jennifer Schopf</a></li>
+
 	<li><a href="http://www-unix.mcs.anl.gov/~franks/">Frank Siebenlist</a></li>
+
         <li><a href="http://www-unix.mcs.anl.gov/~voeckler/">Jens-S. V&ouml;ckler</a>
+
 	<li><a href="http://www-unix.mcs.anl.gov/~wilde/">Mike Wilde</a></li>
+
       </ul>
+
     </td>
+
     </tr>
+
     </table>
+
  
+
 <h2><a href="http://epcc.ed.ac.uk/">EPCC</a>, <a href="http://www.ed.ac.uk/">University of Edinburgh</a></h2>
 
+
+
     <table width="100%">
+
     <tr>
+
     <td width="50%">
+
     <ul>
+
       <li>Mario Antonioletti</li>
+
       <li>Malcolm Atkinson (National e-Science Centre)</li>
+
       <li>Rob Baxter</li>
+
       <li>Ally Hume</li>
+
       <li>Neil Chue Hong</li>
+
     </ul>
+
     </td>
+
     <td width="50%">
+
     <ul>
+
       <li>Mike Jackson</li>
+
       <li>Amy Krause</li>
+
       <li>Mark Parsons</li>
+
       <li>Tom Sugden</li>
+
       </ul>
+
     </td>
+
     </tr>
+
     </table>
+
+
 
 <h2><a href="http://www.ncsa.uiuc.edu/">National Center for Supercomputing Applications (NCSA)</a></h2>
 
+
+
     <ul>
+
     <li><a href="http://www.ncsa.uiuc.edu/~jbasney/">Jim Basney</a></li>
+
     <li>Tom Scavo</li>
+
     <li>Von Welch</li>
+
     </ul>
+
     
+
     
+
 <h2><a href="http://www.hpclab.niu.edu/">Northern
+
     Illinois University, High Performance Computing Laboratory</a></h2>
 
+
+
 	<ul>
+
 	<li><a href="http://www.cs.niu.edu/~karonis/">Nicholas T. Karonis</a></li>
+
         <li><a href="http://www.mcs.anl.gov/~toonen">Brian Toonen</a></li>
+
 	</ul>
+
 	
+
 	
+
 <h2><a href="http://www.kth.se/">Royal Institute of Technology, Sweden</a></h2>
 
+
+
         <ul>
+
         <li><span lang="en-us">Olle Mulmo</span></li>
+
         <li><span lang="en-us">Thomas Sandholm</span></li>
+
         </ul>
+
     
+
     
+
 <h2><a href="http://www.univa.com/">Univa Corporation</a></h2>
+
 	
+
 	<ul>
+
 	<li>Karl Czajkowski</li>
+
 	<li>Bob Gaffaney</li>
+
         <li>Samuel Meder</li>
+
 	<li><a href="http://www.univa.com/about/founders.shtml#stuecke">Steve Tuecke</a></li>
+
 	<li>Rich Wellner</li>
+
 	</ul>
+
 	    
+
     
+
 <h2><a href="http://www.isi.edu">University of Southern California Information Sciences Institute</a></h2>
 
+
+
     <table width="100%">
+
     <tr>
+
     <td width="50%">
+
     <ul>
+
       <li>Shishir Bharathi</li>
+
       <li><a href="http://www.isi.edu/~annc">Ann L. Chervenak</a></li>
+
       <li>Mike D'Arcy</li>
+
       <li><a href="http://www.isi.edu/~deelman">Ewa Deelman</a></li>
+
       <li><a href="http://www.isi.edu/~sridhar">Sridhar Gullapalli</a></li>
+
       <li><a href="http://www.isi.edu/~carl">Carl Kesselman</a></li>
+
         <li><a href="http://www.isi.edu/~gmehta">Gaurang Mehta</a></li>
+
     </ul>
+
     </td>
+
     <td width="50%">
+
     <ul>
+
       <li>Laura Pearlman</li>
+
       <li>Mats Rynge</li>
+
       <li>Robert Schuler</li>
+
       <li>Mei-Hui Su</li>
+
       <li><a href="http://www.isi.edu/~thiebaux">Marcus Thiebaux</a>
+
         <li>Karan Vahi</li>
+
 	<li>Cristina Williams</li>
+
     </ul> 
+
     </td>
+
     </tr>
+
     </table>
+
+
 
 </div>
 
+
+
 <?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
+
