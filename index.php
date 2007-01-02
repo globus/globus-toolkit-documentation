d40355838be51cc2063153cd57a06d237547cce4
liming <liming>
2007-01-02 17:57:16 +0000

    Changed cover image

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
 ?>
 
         <div id="coverimage">
-             <?php include( $SITE_PATHS["SERV_ALLIANCE"]."impact/sea-climate.inc" ); ?>
+             <?php include( $SITE_PATHS["SERV_ALLIANCE"]."impact/scec-1.inc" ); ?>
         </div>
 
         <div id="main">
