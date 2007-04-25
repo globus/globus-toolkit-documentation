5b7af35a2c45c4646ff82bb591d7b372685d18a0
liming <liming>
2007-04-25 18:53:41 +0000

    minor edit

--- privacy/index.php
+++ privacy/index.php
@@ -15,8 +15,8 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
        and the other covers the use of Globus software.</p>
 
     <ul>
-    <li><a href="online-services.php">Globus Online Services Privacy Statement</a></li>
-    <li><a href="software.php">Globus Software Privacy Statement</a></li>
+    <li><a href="online-services.php">Globus Online Services Privacy Statement</a> - Website, email lists, FTP server</li>
+    <li><a href="software.php">Globus Software Privacy Statement</a> - Globus software</li>
     </ul>
 
 <h2>How to Contact Us</h2>
