543bd171ce455452e2b94ac9d1447bc5e8b70053
liming <liming>
2007-04-25 18:51:41 +0000

    Added software privacy statement

--- privacy/index.php
+++ privacy/index.php
@@ -12,13 +12,18 @@ include_once( $SITE_PATHS["SERV_INC"].'header.inc' );
     <p>The following two privacy statements describe the Globus community's practices
        in regard to collecting and using personal information. One statement covers 
        the use of Globus Online Services (websites, email lists, anonymous FTP server) 
-       and the other covers the use of Globus software. These areas are different 
-       enough to merit separate privacy statements.</p>
+       and the other covers the use of Globus software.</p>
 
     <ul>
     <li><a href="online-services.php">Globus Online Services Privacy Statement</a></li>
     <li><a href="software.php">Globus Software Privacy Statement</a></li>
     </ul>
+
+<h2>How to Contact Us</h2>
+
+<p>The Globus community takes all complaints and concerns regarding privacy 
+seriously. Please direct all issues in this area to the email address 
+info@globus.org.</p>
 
 </div>
 
