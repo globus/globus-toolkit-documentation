d00e62da53a64a93753c31c1e1ecc2d14ad4040c
liming <liming>
2007-04-25 17:32:20 +0000

    Initial version of privacy statement index page

---
+++ privacy/index.php
@@ -0,0 +1,25 @@
+<?php
+$title = "Globus: Privacy Statements";
+$section = "section-1";
+include_once( "../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+
+<div id="main">
+ 
+    <h1 class="first">Globus Community Privacy Statements</h1>
+
+    <p>The following two privacy statements describe the Globus community's practices
+       in regard to collecting and using personal information. One statement covers 
+       the use of Globus Online Services (websites, email lists, anonymous FTP server) 
+       and the other covers the use of Globus software. These areas are different 
+       enough to merit separate privacy statements.</p>
+
+    <ul>
+    <li>Globus Online Services Privacy Statement</li>
+    <li>Globus Software Privacy Statement</li>
+    </ul>
+
+</div>
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
