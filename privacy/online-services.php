ce74dbb1a408f131d5ab430b3479b4daef95c17f
liming <liming>
2007-04-25 18:12:12 +0000

    Final version approved by GMC on April 25, 2007

---
+++ privacy/online-services.php
@@ -0,0 +1,140 @@
+<?php
+$title = "Globus: Globus Online Services Privacy Statement";
+$section = "section-1";
+include_once( "../include/local.inc" );
+include_once( $SITE_PATHS["SERV_INC"].'header.inc' ); 
+?>
+
+<div id="main">
+ 
+    <h1 class="first">Globus Online Services Privacy Statement</h1>
+
+    <p>Status: CURRENT</p>
+
+<p>The following statement describes data collection, use, and sharing practices 
+within the Globus community in regard to use of the Globus community's online 
+services (websites, email lists, anonymous FTP server). A 
+<a href="software.php">related statement</a> 
+covers practices related to data collection, use, and sharing in regard to use of 
+Globus software.</p>
+
+<h2>Data We Collect</h2>
+
+<p>The Globus websites (www.globus.org, dev.globus.org, bugzilla.globus.org) and 
+our anonymous FTP server collect and log usage data in the normal course of 
+operation. The web servers that host these sites collect basic data about the 
+browsers used to access them (IP address and any HTTP header data provided by the 
+browser). They may use cookies and/or Web beacons to track usage patterns. The 
+information collected via these means is used for no purpose other than to track 
+and analyze aggregate usage patterns in order to improve the quality of the 
+websites and to customize the website experience for users (e.g., remembering the 
+contents of fill-out forms for reuse in subsequent forms). Most browsers provide 
+options for disabling cookies, and you are free to use these options, though it 
+may cause your experience of our websites to be different from users who do not 
+use these options.</p>
+
+<p>Note that Globus websites may use the 
+<a href="http://www.google.com/analytics/">Google Analytics</a> service (provided by 
+Google) to track and analyze website usage. The use of Google Analytics involves 
+use of a cookie and requires us to share anonymous usage data with Google. For 
+Google's privacy policy, click 
+<a href="http://www.google.com/privacypolicy.html">here</a>.</p>
+
+<p>Our websites use fill-out forms for requesting information when you engage in 
+certain activities. For example, when you download the Globus Toolkit, we request 
+data about you. This is also true for our anonymous FTP server. It is your choice 
+whether you enter valid data, enter invalid data, or enter no data. We make no 
+attempt to validate this data, and you may download the software without 
+providing any data, valid or not.</p>
+
+<p>Some of our websites (Bugzilla, the dev.globus Wiki) allow you to register 
+your identity by creating a user name and password. In some cases, you will not 
+be permitted to take certain actions (enter data, edit content) without 
+registering. The registration process may include entering a valid email address, 
+and if you do not enter a valid address, your registration will not succeed. Any 
+data that you enter when registering with these sites is collected and logged. 
+Your user name will typically be visible to others. Some of our websites 
+(Bugzilla, the dev.globus Wiki) allow registered users to enter data and/or edit 
+content. The data and content that you supply is logged and archived and is 
+typically made available for others to view. Even if you remove the data (e.g., 
+by editing a Wiki page), the original data is retained and available for review 
+by others.</p>
+
+<p>The Globus community uses an extensive set of electronic mailing lists. When 
+you subscribe to any of these mailing lists, your email address is stored. If 
+you include your full name or any other data when signing up for a mailing 
+list, that information is also stored. Any information that you post to a 
+Globus mailing list (including header information such as your email address 
+and other personal information) will most likely be archived both by Globus and 
+by other subscribers and made available for viewing by others. Use of Globus 
+software also may result in collection and storage of personal information 
+and/or usage data. See the <a href="software.php">Globus Software Privacy 
+Statement</a> for details.</p>
+
+<h2>How This Data Is Used</h2>
+
+<p>IP addresses, HTTP header information, cookie and Web beacon data collected 
+by Globus websites is logged and used specifically for the purpose of tracking 
+and analyzing aggregate usage patterns. We do not attempt to identify or track 
+specific users. Usage tracking and analysis is performed in order to help us 
+improve the quality and usability of the websites. The data that you provide 
+when requested by our web servers (e.g., the Globus Toolkit download form) is 
+used to generate reports on the community participation for our own use 
+(understanding the community and identifying possible areas for community 
+expansion) and--in aggregate form--for use by our sponsors and funders 
+toward understanding the impact of their sponsorship. We may identify 
+individual users in this data, if you supply the data to enable this.</p>
+
+<p>Data and content that you enter in Bugzilla and the dev.globus Wiki is 
+shared publicly with the community.</p>
+
+<p>Email addresses that you enter when subscribing to Globus mailing lists are 
+used to distribute messages posted to those lists to you. We may also use 
+these addresses to analyze the subscriber base to better understand aggregate 
+Globus community participation. We do not share mailing list subscriber lists 
+or email addresses with third parties. However, if you post to a Globus mailing 
+list, your email address and any other personal information that you include in 
+your postings may be stored by other subscribers and used at their discretion.</p>
+
+<p>We may combine IP addresses and email addresses with other sources of 
+information (e.g., domain name system (DNS), the whois registry) to assist us 
+in analyzing use patterns of our websites and mailing lists.</p>
+
+<p>The web server logs, FTP server logs, and mailing list administrative logs 
+are available only to authorized members of the Globus community. Because this 
+is a global community, this means that the data will be transported across 
+international boundaries.</p>
+
+<h2>Treatment of Spam</h2>
+
+<p>The Globus community is sensitive to the need to avoid unsolicited 
+and off-topic messages on our community email lists. The hosting service that 
+we use for our email lists has been configured to help in this regard in several 
+ways. Most Globus email lists are configured to prevent postings from any address 
+that is not subscribed to the list. Such messages are rejected immediately and 
+never forwarded to subscribers. All mailing lists are configured to disable 
+access to the subscriber list. The email hosting service is also configured to 
+evaluate messages for virus and spam content and to flag such messages with 
+headers so that email clients can automatically separate them from legitimate 
+messages. Due to the global nature of our community, it is impossible to avoid 
+all off-topic content on our mailing lists, and automatic rejection of off-topic 
+messages is risky. We are constantly reevaluating our strategies for handling 
+spam and addressing new spamming techniques.</p>
+
+<h2>Changes to This Statement</h2>
+
+<p>We may occasionally revise the Globus Online Services Privacy Statement. 
+Changes to the statement will appear on this page. Please note that our practices 
+with respect to data collected and used in connection with this website and all 
+other Globus online services are governed by this statement as amended from time 
+to time, and not the statement in effect at the time the data was collected.</p>
+
+<h2>How to Contact Us</h2>
+
+<p>The Globus community takes all complaints and concerns regarding privacy 
+seriously. Please direct all issues in this area to the email address 
+info@globus.org.</p>
+
+</div>
+
+<?php include($SITE_PATHS["SERV_INC"].'footer.inc'); ?>
