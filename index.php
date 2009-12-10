d391ba1d5ff728a315c854fe8463444650f7f036
cristina <cristina>
2009-12-10 17:34:51 +0000

    fixing link to rss feed

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ define("FOOTER_INC",1); //skip the footer
             	 include_once( $SITE_PATHS["SERV_ROOT"]."include/globus_news.php");
              	globus_print_alliance_news_headlines(3);
           	?>
-			<p><a href="http://www-unix.globus.org/toolkit/rss/downloadNews/downloadNews.xml"><img src="http://www-unix.globus.org/toolkit/images/iconRSS.gif"  width="35" height="13" border="0"></a>&nbsp;<a href="/toolkit/about_rss.html">What's&nbsp;this?</a></p>            
+			<p><a href="http://www.globus.org/toolkit/rss/downloadNews/downloadNews.xml"><img src="http://www.globus.org/toolkit/images/iconRSS.gif"  width="35" height="13" border="0"></a>&nbsp;<a href="/toolkit/about_rss.html">What's&nbsp;this?</a></p>            
         </div>
     </div>
 </div>
