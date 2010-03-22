7d5bd03e017aff142fc7c8ddc8ebf7e018475392
raffenet <raffenet>
2010-03-22 14:18:54 +0000

    fixed broken rss generation

--- toolkit/rss/downloadNews/downloadNews.php
+++ toolkit/rss/downloadNews/downloadNews.php
@@ -1,4 +1,4 @@
 <?php
-    include_once( "/mcs/www-unix.globus.org/include/globus_news.php" );
+    include_once( "/mcs/globus.org/include/globus_news.php" );
     globus_print_toolkit_news_rss20(50); 
 ?>
